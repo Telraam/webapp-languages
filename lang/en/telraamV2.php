463e8a3a76d08e87de048955c3917ae3090f79f3
Dave Driesmans <dave@waanz.in>
2022-10-19 10:47:15 +0200

    V2 clean up

--- lang/en/telraamV2.php
+++ lang/en/telraamV2.php
@@ -6,36 +6,31 @@ return [
 
     'subtitle'          => 'Introducing our new sensor - the Telraam S2<br>Small, smart, and powerful<br>Effortless traffic counting on every street',
 
-    'section1-title'       => '',
-    'section1-block1-text'        => 'This is the ideal solution, whether it is for individuals and campaigners, or local authorities and mobility professionals in SMART cities, for gathering detailed information on traffic on all streets. Track multiple modes simultaneously with the S2 to see the impact of any interventions or changes in local habits. <br><br>Learn more about the features of the Telraam S2 below, check out the packages available, and get in touch to discuss the ideal solution for your needs.',
-
-    'section2-title'       => '',
-    'section2-block1-text'        => '',
+    'section1-block1-text'        => 'This is the ideal solution, whether it is for individuals and campaigners, or local authorities and mobility professionals in SMART cities, for gathering
+    detailed information on traffic on all streets. Track multiple modes simultaneously with the S2 to see the impact of any interventions or changes in local habits.',
+    'section1-block2-text'        => ' Learn more about the features of the Telraam S2 below, check out the packages available, and get in touch to discuss the ideal solution for your needs.',
 
     'section-features-title'       => 'Features',
 
    'section-features-block1-title'  => 'Smart',
     'section-features-block1-subtitle'     => 'Streets are not just for cars.',
     'section-features-block1-body'     => '<p>The Telraam S2 device counts, categorises and reports all traffic on your street: cars, bikes, pedestrians, buses, trucks and more. Get an accurate picture of who uses the street, when and how and start to have meaningful conversations about local traffic and mobility plans.',
-    'section-features-block1-hidden'    => '<p></p>',
 
     'section-features-block2-title'  => 'Robust',
     'section-features-block2-subtitle'     => 'A brand new device for complete autonomy.',
     'section-features-block2-body'     => '<p>Telraam S2 has been built from scratch, and is fundamentally different to our Raspberry Pi based sensor</p><p><ul>
-		<li>Custom-built PCB using [established] components</li>
+      <li>Custom-built PCB using [established] components</li>
         <li>Integrated AI</li>
         <li>IoT connectivity</li>
         <li>OTA firmware updates</li>
         <li>Remote management</li>
         </ul></p>',
-    'section-features-block1-hidden'    => '<p></p>',
 
     'section-features-block3-title' => 'Elegant',
     'section-features-block3-subtitle'     => 'Modern design with functionality and user experience in mind.',
     'section-features-block3-body' => '<p>A design that fits effortlessly and quietly into your life, and your window view.</p>
         <p>The device is simply powered via a USB-C port, has a single button for user interaction, and comes with a colour LCD display. The screen is not backlit, so will not bother you whether in a living room or bedroom, and to save even more energy, can be turned off completely.</p>
         <p>Telraam S2 will be sold in 100% recyclable paper packaging, with no plastic waste.</p>',
-    'section-features-block3-hidden'     => '<p></p>',
 
     'section-features-block4-title' => 'Simple',
     'section-features-block4-subtitle'     => 'Five minute installation - no rocket science degree required.',
@@ -43,25 +38,23 @@ return [
         <p>Telraam S2 will work like any smart plug-and-play device. Just place your Telraam S2 on the inside of your window using the mount provided, connect the USB-C cable, scan a code with your smartphone*, and complete the registration on your screen. No fiddling with wifi passwords, or adjusting and controlling the camera angle. Within 15 minutes of your registration, your Telraam S2 will be live on the map! </p>
         <p>The new LCD screen not only helps during installation but it also provides you with ongoing traffic updates for the day and the past quarter of an hour.</p>
         <p class=“tiny”>* Use the QR code provided or enter a code in any browser</p>',
-    'section-features-block4-hidden'     => '<p></p>',
 
     'section-features-block5-title' => 'Precision & Privacy',
     'section-features-block5-subtitle'     => 'AI for accuracy and precision, night counts, and anonymous data.',
     'section-features-block5-body' => '<p>Telraam S2 uses a completely new counting methodology, based on Artificial Intelligence (AI) and Neural Networks. This enables it to be consistent, precise and accurate, and even provide vehicle counts in dark conditions.</p>
         <p>Privacy is very important for us at Telraam. No images or videos are ever saved or transferred from our devices. With Telraam S2, even the classification of objects detected happens in situ on the edge-devices. As a result, only the anonymised 15 minute aggregate count data is transferred to the Telraam database.</p>',
-    'section-features-block5-hidden'     => '<p></p>',
+
+
+    'section-features-block4-title'  => 'What does it cost?',
 
 
     'section-features-block6-title' => 'Make it count!',
     'section-features-block6-subtitle'     => 'A solution for every street.',
     'section-features-block6-body' => '<p>Whether you’re a private citizen in need of a single device for your street, or a professional looking to setup a whole network in your City, Telraam is there to help. Each device is ready to start counting. Packages include tools for building and managing local networks, whether for a neighbourhood or a city. Check out the options that best suit you below to get started.</p>',
-    'section-features-block6-hidden'     => '<p></p>',
-
-    'section3-title'       => 'Pricing',
-    'section3-body'       => '',
-
 
     'section4-title'   => 'Get in Touch!',
     'section4-body'    => 'Sign up to receive the latest updates on Telraam S2'
+
+
 
 ];
