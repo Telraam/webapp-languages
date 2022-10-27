ad91c8111bae7148f56e2f9127deb39df23a9b25
Péter I. Pápics <papics@gmail.com>
2022-10-27 14:45:05 +0200

    Update telraamV2.php
    - There is no need to say "Introducing our new sensor - the Telraam S2" when the title of the page is Telraam S2. Just "Introducing our new sensor" is perfect.
    - A few Oxford commas added
    - Added data-driven to meaningful conversations
    - when and how -> discover patterns and anomalies
    - "A robust and reliable tool that will keep counting as long as you need it to." Replaced by a more informative sentence.
    - List under HW features changed
    - Period from any title end removed
    - Updated independently -> remotely
    - Instructions slightly improved
    - Other small textual changes
    - Tech specs table very much updated

--- lang/en/telraamV2.php
+++ lang/en/telraamV2.php
@@ -4,7 +4,7 @@ return [
 
     'title'           => 'Telraam S2',
 
-    'subtitle'          => 'Introducing our new sensor - the Telraam S2<br>Small, smart, and powerful<br>Effortless traffic counting on every street',
+    'subtitle'          => 'Introducing our new sensor<br>Small, smart, and powerful<br>Effortless traffic counting on every street',
 
     'section1-block1-text'        => 'This is the ideal solution, whether it is for individuals and campaigners, or local authorities and mobility professionals in SMART cities, for gathering
     detailed information on traffic on all streets. Track multiple modes simultaneously with the S2 to see the impact of any interventions or changes in local habits.',
@@ -14,68 +14,68 @@ return [
 
     'section-features-block1-title'  => 'Multimodal',
     'section-features-block1-subtitle'     => 'Streets are not just for cars',
-    'section-features-block1-body'     => '<p>The Telraam S2 device counts, categorises and reports all activity on your street: cars, bikes, pedestrians, buses, trucks and more. Get an accurate picture of who uses the street, when and how and start to have meaningful conversations about local traffic and mobility plans.',
+    'section-features-block1-body'     => '<p>The Telraam S2 device counts, categorises, and reports all activity on your street: cars, bikes, pedestrians, buses, trucks, and more. Get an accurate picture of who uses the street, discover patterns and anomalies, and start to have meaningful data-driven conversations about local traffic and mobility plans.',
 
     'section-features-block2-title'  => 'Purpose-built',
     'section-features-block2-subtitle'     => 'A brand new device for complete autonomy',
-    'section-features-block2-body'     => '<p>Telraam S2 has been built from scratch for the specific purpose of counting traffic. A robust and reliable tool that will keep counting as long as you need it to.</p>
+    'section-features-block2-body'     => '<p>Telraam S2 has been designed and built from scratch for the specific purpose of counting traffic. It is robust, reliable, and it will operate completely autonomously as long as you need it to thanks to the built in mobile data connection, and the remote device management tools.</p>
         <p><ul>
-        <li>Custom-built PCB</li>
-        <li>Integrated AI chip</li>
-        <li>Internet-of-Things (IoT) connectivity</li>
-        <li>OTA firmware updates</li>
-        <li>Remote management</li>
-        <li>Dashboard integrations</li>
+        <li>Custom-built PCB with designated AI chip</li>
+        <li>Integrated mobile data connection</li>
+        <li>Internet-of-Things (IoT) framework</li>
+        <li>Remote device management</li>
+        <li>OAT firmware updates (AI and system)</li>
+        <li>Dashboard solutions</li>
         </ul></p>',
 
     'section-features-block3-title' => 'Simple',
-    'section-features-block3-subtitle'     => 'Five minute installation - no rocket science degree required.',
-    'section-features-block3-body' => '<p>The Telraam S2 is a plug-and-play device. After a simple set-up, it runs continuously, sending data without the need for wifi, and can be updated independently.</p>
-        <p>Place your Telraam S2 on the inside of your window using the mount provided, connect the USB-C cable, scan a code with your smartphone*, and complete the registration on your screen. No fiddling with wifi passwords, or adjusting and controlling the camera angle. Within 15 minutes of registration, your Telraam S2 will be live on the map!</p>
-        <p>The LCD screen not only helps during installation but also provides ongoing traffic updates for the day and the past quarter of an hour.</p>
+    'section-features-block3-subtitle'     => 'Five minute installation - no rocket science degree required',
+    'section-features-block3-body' => '<p>The Telraam S2 is a plug-and-play device. After a simple set-up, it runs continuously, sending data without the need for wifi, and can be updated remotely.</p>
+        <p>Place your Telraam S2 on the inside of your window using the self-adhesive mount provided, connect the USB-C cable, scan a code with your smartphone*, and complete the registration on your screen. No fiddling with wifi passwords, or adjusting and controlling the camera angle. Within 15 minutes of registration, your Telraam S2 will be live on the map!</p>
+        <p>The LCD screen not only helps during installation but also provides an overview of the actual traffic for the day and the past quarter of an hour.</p>
         <p class=tiny>* Use the QR code provided or enter a code in any browser</p>',
 
     'section-features-block4-title' => 'Elegant',
     'section-features-block4-subtitle'     => 'Modern design with functionality and user experience in mind',
     'section-features-block4-body' => '<p>A design that fits effortlessly and quietly into your life, and your window view.</p>
-        <p>The device is powered via a USB-C port, has a single button for user interaction, and comes with a colour LCD display. The screen can be turned off completely, which saves more energy, and so will not bother you whether in a living room or bedroom window.</p>
+        <p>The device is powered via a USB-C port, has a single button for user interaction, and comes with a colour LCD display. The screen can be turned off completely, which saves energy, and stops any potentially unwanted light or movement coming from your Telraam S2 - whether in a living room or bedroom window.</p>
         <p>Telraam S2 is sold in 100% recyclable paper packaging, with no plastic waste.</p>',
 
     'section-features-block5-title' => 'Precision',
-    'section-features-block5-subtitle'     => 'AI for accuracy and precision, night counts, and anonymous data',
-    'section-features-block5-body' => '<p>Telraam S2 uses a completely new counting methodology, based on Artificial Intelligence (AI) and Neural Networks. This enables it to be consistent, precise and accurate, and even provide vehicle counts in dark conditions.</p>',
+    'section-features-block5-subtitle'     => 'AI for accuracy, precision, night counts, and anonymous data',
+    'section-features-block5-body' => '<p>Telraam S2 uses a completely new counting methodology compared to our first Raspberry Pi-based, using Artificial Intelligence (AI) and Neural Networks. This results in an improved consisteny, precision, and accuracy, even providing vehicle counts under dark conditions.</p>',
 
     'section-features-block6-title' => 'Privacy',
     'section-features-block6-subtitle'     => 'Object detection not identification',
-    'section-features-block6-body' => '<p>Privacy is very important for us at Telraam. No identifiable images or videos are saved or transferred from our devices. With Telraam S2, the classification of any object detected happens in situ on the edge-devices. As a result, only the anonymised 15-minute aggregate count data is transferred to the Telraam database.</p>',
+    'section-features-block6-body' => '<p>Privacy is very important for us at Telraam. No identifiable images or videos are saved or transferred from our devices. With Telraam S2, the classification of any detected object happens in situ on the edge-devices. As a result, only the anonymised 15-minute aggregate counts are transferred to the Telraam database.</p>',
 
     'section-features-block7-title' => 'Make it count!',
-    'section-features-block7-subtitle'     => 'A solution for every street.',
-    'section-features-block7-body' => '<p>Whether you’re a private citizen in need of a single device for your street, or a professional looking to setup a whole network in your City, Telraam is there to help. Each device is ready to start counting. Packages include tools for building and managing local networks, whether for a neighbourhood or a city. Check out the options that best suit you below to get started.</p>',
+    'section-features-block7-subtitle'     => 'A solution for every street',
+    'section-features-block7-body' => '<p>Whether you are a private citizen in need of a single device for your street, or a professional looking to setup a whole network in your city, Telraam is there to help. Each device is ready to start counting. Packages include tools for building and managing local networks, whether for a small neighbourhood or a metropolis. Check out the options that best suit you below to get started.</p>',
 
     // tech table
     'spec-table-title' => 'Technical specifications',
 
-    'spec-table-row-1-subject'     => 'Chip',
-    'spec-table-row-1-details' => 'Kendryte K210 Processor',
+    'spec-table-row-1-subject'     => 'AI',
+    'spec-table-row-1-details' => 'K210 edge computing chip',
 
     'spec-table-row-2-subject'     => 'Connection',
     'spec-table-row-2-details' => 'MQTT w/ thingstream IoT<br>No wifi connection needed',
 
     'spec-table-row-3-subject'     => 'Camera',
-    'spec-table-row-3-details' => 'Lorem ipsum dolor',
+    'spec-table-row-3-details' => 'Wide anngle fixed lens<br>Automatic region of interest selection',
 
     'spec-table-row-4-subject'     => 'Size',
-    'spec-table-row-4-details' => '76 x 50 x 25',
+    'spec-table-row-4-details' => '65 mm x 99 mm x 30 mm (W x H x D)',
 
     'spec-table-row-5-subject'     => 'Power',
     'spec-table-row-5-details' => '< 300mW<br>',
 
     'spec-table-row-6-subject'     => 'Privacy',
-    'spec-table-row-6-details' => 'Counting AI on the device. <br>No videos are sent to the Telraam <br>',
+    'spec-table-row-6-details' => 'Counting AI on the device<br>No videos or images are saved<br>',
 
     'spec-table-row-7-subject'     => 'Security',
-    'spec-table-row-7-details' => 'Read - only memory（OTP)',
+    'spec-table-row-7-details' => 'Read-only memory（OTP)<br>FW can only be changed using a secore-key',
 
     'spec-table-row-8-subject'     => 'Includes',
     'spec-table-row-8-details' => 'USB-C Cable<br> Adapter',
