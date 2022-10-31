2ad10a306cfff091151782418527674edeea0e3d
Dave Driesmans <dave@waanz.in>
2022-10-31 19:04:24 +0100

    Contact us update

--- lang/en/public-contact-us.php
+++ lang/en/public-contact-us.php
@@ -3,11 +3,10 @@
 return [
 
     'title'       => 'Contact us',
-
-
+    
     'intro'       => 'Thank you for your interest in Telraam. We’d love to hear from you and will reply as soon as we can.',
 
-    'text-above-form'       => 'Please complete the form below and let us know what you would like to discuss so the message
-     can be forwarded to the appropriate member of the team.',
+    'text-above-form'       => 'Please select and complete the right form below. Your message will be forwarded to
+     the team and we\'ll reply as soon as possible.',
 
 ];
