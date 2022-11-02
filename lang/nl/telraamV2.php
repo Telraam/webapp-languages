ef24eb6b4969db6bd9eee27e52b3010e6a42057c
elkeverhoeven <elke.verhoeven@telraam.net>
2022-11-02 17:27:12 +0100

    TelraamV2_NL

---
+++ lang/nl/telraamV2.php
@@ -0,0 +1,95 @@
+<?php
+
+return [
+
+    'title'           => 'Telraam S2',
+
+    'subtitle'          => 'Onze nieuwe sensor - Beschikbaar vanaf maart 2023<br>Subtiel, slim, and doeltreffend<br>Moeiteloos verkeer tellen in elke straat',
+
+    'section1-block1-text'        => '<p><strong>De ideale oplossing om gedetailleerde informatie over het verkeer te verzamelen</strong> op iedere straat, zowel voor particulieren en campagnevoerders als voor lokale overheden en mobiliteitsprofessionals in SMART-steden. </p>
+    <p><strong>Volg meerdere modaliteiten tegelijk</strong> met de S2 om het effect te zien van eventuele ingrepen of veranderingen in lokale gewoonten.</p>',
+    'section1-block2-text'        => '<p>Lees hieronder meer over de functies van de Telraam S2, bekijk de beschikbare opties en neem contact op met ons. Wij helpen je om de geschikte oplossing voor jouw behoeften te kiezen.</p>',
+
+    'section-features-title'       => 'Kenmerken',
+
+    'section-features-block1-title'  => 'Multimodaal',
+    'section-features-block1-subtitle'     => 'De straat, niet enkel voor auto's,
+    'section-features-block1-body'     => '<p>THet Telraam S2-apparaat telt, categoriseert en geeft de drukte op straat weer: het aantal auto\'s, fietsen, voetgangers, bussen, vrachtwagens en meer.</p><p> Krijg een nauwkeurig beeld van wie de straat gebruikt, ontdek patronen en anomalieën, en start zinvolle datagestuurde gesprekken over lokale verkeers- en mobiliteitsplannen.',
+
+    'section-features-block2-title'  => 'Speciaal voor ons gebouwd',
+    'section-features-block2-subtitle'     => 'Een toestel voor volledige autonomie',
+    'section-features-block2-body'     => '<p>Telraam S2 is volledig nieuw ontworpen en gebouwd met het specifieke doel om verkeer te tellen. Het is robuust en betrouwbaar en werkt volledig autonoom dankzij de ingebouwde mobiele dataverbinding en de tools voor remote toestelbeheer.</p>
+        <p><ul class="jump-in">
+        <li>Op maat gemaakte PCB met AI-chip</li>
+        <li>Geïntegreerde mobiele dataverbinding</li>
+        <li>Internet-of-Things (IoT) framework</li>
+        <li>Toestelbeheer op afstand</li>
+        <li>OAT-firmware-updates (AI en systeem)</li>
+        <li>Dashboard oplossingen</li>
+        </ul></p>',
+
+    'section-features-block3-title' => 'Simpel',
+    'section-features-block3-subtitle'     => 'Vijf minuten installation',
+    'section-features-block3-body' => '<p>Telraam S2, plug en play</p>
+        <p>Na een eenvoudige set-up werkt het toestel, verstuurt het gegevens zonder dat wifi nodig is, en kan het op afstand worden geüpdated.</p>
+        <p>Plaats je Telraam S2 aan de binnenkant van je raam met de meegeleverde zelfklevende houder, sluit de USB-C-kabel aan, scan een code met je smartphone* en voltooi de registratie op je scherm. Geen gedoe met wifi-wachtwoorden, of het aanpassen en regelen van de camerahoek. Binnen 15 minuten na registratie staat je Telraam S2 live op de kaart!</p>
+        <p>Het LCD-scherm helpt niet alleen bij de installatie, maar geeft ook een overzicht van het actuele verkeer van de dag en het afgelopen kwartier.</p>
+        <p class=tiny>* Scan de meegeleverde QR-code of voer een code in in eender welke browser</p>',
+
+    'section-features-block4-title' => 'Elegant',
+    'section-features-block4-subtitle'     => 'Nieuw ontwerp met functionaliteit en gebruiksvriendelijkheid als leidraad',
+    'section-features-block4-body' => '<p>Een ontwerp dat in je dagelijks leven, omgeving en meer specifiek op het uitzicht op je raam goed staat.</p>
+        <p>Het apparaat wordt gevoed via een USB-C-poort, heeft één knop voor gebruikersinteractie en wordt geleverd met een LCD-kleurenscherm. Het scherm kan volledig worden uitgeschakeld, wat energie bespaart en voorkomt dat er mogelijk ongewenst licht van uw Telraam S2 komt - of het nu in een woonkamer of slaapkamer is.</p>
+        <p>Telraam S2 wordt verkocht in 100% recyclebare papieren verpakkingen, zonder plastic afval.</p>',
+
+    'section-features-block5-title' => 'Nauwkeurigheid',
+    'section-features-block5-subtitle'     => 'AI voor nauwkeurigheid, precisie, nachttellingen en anonieme gegevens',
+    'section-features-block5-body' => '<p>Telraam S2 gebruikt een volledig nieuwe telmethode in vergelijking met onze eerste Raspberry Pi-gebaseerd toestel, dit met behulp van kunstmatige intelligentie (AI) en neurale netwerken. Dit resulteert in een verbeterde precisie en nauwkeurigheid, en ook bij donkere omstandigheden voor gemotoriseerde voertuigen.</p>',
+
+    'section-features-block6-title' => 'Privacy',
+    'section-features-block6-subtitle'     => 'Objectdetectie, geen identificatie',
+    'section-features-block6-body' => '<p>Privacy is erg belangrijk voor ons bij Telraam. Er worden geen identificeerbare beelden of video\'s opgeslagen of overgedragen vanaf onze apparaten. Met Telraam S2 gebeurt de classificatie van elk gedetecteerd object ter plaatse op de toestellen. Als gevolg daarvan worden alleen de geanonimiseerde 15-minuten tellingen overgebracht naar de Telraam database.</p>',
+
+    'section-features-block7-title' => 'Tel mee!',
+    'section-features-block7-subtitle'     => 'Een oplossing voor elke straat',
+    'section-features-block7-body' => '<p>Of je nu een particulier bent die een enkel toestel nodig heeft voor uw straat, of een professional die een heel netwerk in uw stad wil opzetten, Telraam is er om u te helpen. Je kan ook kiezen voor hulpmiddelen voor het bouwen en beheren van lokale netwerken, of het nu gaat om een kleine wijk of een metropool. Bekijk hieronder de optie die het beste bij jou past om aan de slag te gaan.</p>',
+
+    // tech table
+    'spec-table-title' => 'Technische specificatiets',
+
+    'spec-table-row-1-subject'     => 'AI',
+    'spec-table-row-1-details' => 'K210 edge computing chip',
+
+    'spec-table-row-2-subject'     => 'Connectie',
+    'spec-table-row-2-details' => 'MQTT met thingstream IoT<br>Geen wifi verbinding nodig',
+
+    'spec-table-row-3-subject'     => 'Camera',
+    'spec-table-row-3-details' => 'Vaste groothoeklens<br>Automatische selectie van het interessegebied',
+
+    'spec-table-row-4-subject'     => 'Grootte',
+    'spec-table-row-4-details' => '65 mm x 99 mm x 30 mm<br>(B x H x D)',
+
+    'spec-table-row-5-subject'     => 'Power',
+    'spec-table-row-5-details' => '< 300 mW<br>',
+
+    'spec-table-row-6-subject'     => 'Privacy',
+    'spec-table-row-6-details' => 'AI telt op het apparaat<br>Er worden geen video\'s of afbeeldingen opgeslagen<br>',
+
+    'spec-table-row-7-subject'     => 'Security',
+    'spec-table-row-7-details' => 'Read-only geheugen (OTP) <br>FW kan alleen worden gewijzigd met een beveiligde sleutel.',
+
+    'spec-table-row-8-subject'     => 'Inclusief',
+    'spec-table-row-8-details' => 'USB-C kabel<br> Adapter',
+
+
+    // pricing
+    // pricing content in sep. lang file
+    'section-features-block-pricing'  => 'Prijzen',
+
+    // modal
+    'modal-title'    => 'Meld je aan om de nieuwste Telraam S2 updates te ontvangen',
+    'modal-body'    => 'Vul dit korte formulier in en iemand van Telraam zal contact met je opnemen'
+
+
+
+];
