921ccce8d9ddd1a7d06e705f890e6abd8954bd4e
elkeverhoeven <elke.verhoeven@telraam.net>
2022-11-03 15:51:13 +0100

    pricing_NL & FR

---
+++ lang/fr/pricing.php
@@ -0,0 +1,115 @@
+<?php
+
+return [
+
+    'title' => 'Pris',
+    'subtitle' => 'Permettre aux citoyens ou aux professionnels de piloter des projets de mobilité.',
+
+    // ------------------------------------------------------
+    // ------------------- block #1
+    // ------------------------------------------------------
+    'title-block-1' => 'Aperçu général',
+
+    'pack-individual-title' => 'Individuel',
+    'pack-individual-subtitle' => 'Pour observer un endroit',
+    'pack-individual-number' => '1 appareil',
+
+    'pack-standard-title' => 'Standard',
+    'pack-standard-subtitle' => 'Des outils puissants pour gérer un réseau local',
+    'pack-standard-number' => 'Jusqu\'à 50 appareils',
+
+    'pack-enhanced-title' => 'Enhanced',
+    'pack-enhanced-subtitle' => 'Gagnez du temps grâce à des outils d\'analyse, de rapport et de gestion de premier ordre pour des projets plus complets',
+    'pack-enhanced-number' => 'Jusqu\'à 50 appareils',
+
+    'pack-custom-title' => 'Personnalisé',
+    'pack-custom-subtitle' => 'Des solutions pour les besoins spécifiques des grands projets',
+    'pack-custom-number' => '50+ appareils',
+
+    'per-device' => 'Par appareil',
+    'incl-vat' => '(incl BTW)',
+    'excl-vat' => '(ex BTW)',
+    'one-time-hardware' => 'Coût unique du hardware',
+    'month' => 'mois',
+    'subscription' => 'Abonnement',
+    'network-subscription' => 'Abonnement au réseau, facturé annuellement',
+    'tailer-made' => 'Solutions personnalisées',
+    'free' => 'Gratuit',
+
+    // buttons
+    'buy-a-telraam' => 'Acheter un Telraam',
+    'get-started' => 'Commencer',
+    'get-in-touch' => 'Contactez-nous',
+
+    // includes
+    'include-title' => 'Y compris:',
+    'individual-include-1' => 'Tableau de bord personnel',
+    'individual-include-2' => 'Accès basique à l\'API',
+    'individual-include-3' => 'Support par e-mail',
+
+    'standard-include-1' => 'Toutes les caractéristiques de l\'option Individuelle',
+    'standard-include-2' => 'Tableau de bord du réseau',
+    'standard-include-3' => 'Support prioritaire',
+
+    'enhanced-include-1' => 'Toutes les caractéristiques de Standard',
+    'enhanced-include-2' => 'Outils d\'analyse de niveau supérieur',
+    'enhanced-include-3' => 'Rapporteurs de niveau supérieur',
+    'enhanced-include-4' => 'Gérer plusieurs trajets',
+
+    'custom-include-1' => 'Toutes les fonctionnalités de Enhanced',
+    'custom-include-2' => 'Une image de marque sur mesure',
+    'custom-include-3' => 'Soutien du projet',
+
+    // ------------------------------------------------------
+    // ------------------- block #2
+    // ------------------------------------------------------
+    'title-block-2' => 'Quelles sont les fonctionnalités offertes dans chaque paquet?',
+
+    'pricing-per-device' => 'Prix par appareil',
+    'subscription-software' => 'Abonnement aux logiciels',
+    'one-time-hardware-S2' => 'Coût unique du matériel (Telraam S2)',
+
+    'usage' => 'Utilisation',
+    'who-is-this-for' =>  'Pour qui',
+    'individual-citizens' => 'Citoyens individuels',
+    'professionals-campaigners' => 'Professionnels et activistes',
+    'professionals' =>  'Professionnels',
+
+    'features' => 'Fonctionnalités',
+    'personal-dashboard' => 'Tableau de bord personnel',
+    'network-dashboard' => 'Tableau de bord du réseau',
+    'network-dashboard-feature-1'  => 'Gérer les paramètres du réseau',
+    'network-dashboard-feature-2' => 'Sélection des candidats',
+    'network-dashboard-feature-3' => 'Emails automatisés ',
+    'network-dashboard-feature-4' => 'Outils d\'analyse de base',
+    'network-dashboard-feature-5' => 'Outils analytiques avancés',
+    'network-dashboard-feature-6' => 'Options de marquage',
+    'network-dashboard-feature-7' => 'Plusieurs trajets',
+
+    'api-access' => 'Accès à l\'API',
+    'api-for-one-hour' => 'Accès de base à l\'API pour les données d\'une heure',
+    'api-15min' => 'Accès avancé à l\'API: données de 15 minutes, données sur le trafic typique, rapport sur les rues.',
+    'api-custom'  => 'Personnalisé: options API sur demande, limites tarifaires personnalisées',
+
+    'support' => 'Support',
+    'support-faq' => 'Articles de support FAQ',
+    'support-tt' => 'Support entre pairs grâce à Telraam Talks',
+    'support-mail-low' => 'Assistance par e-mail <br> (réponse dans les 3 à 5 jours ouvrables)',
+    'support-mail-high' => 'Assistance par e-mail prioritisé <br>(réponse dans 1 jour ouvrable)',
+    'support-training' => 'Sessions de formation<br><i>“comment gérer mon réseau"</i>',
+    'support-training-2h' => '2 x 1h (1-sur-1)',
+    'support-training-4h' => '4 x 1h (1-sur-1)',
+    'support-training-on-request' => 'Sur demande',
+
+    'services' => 'Services',
+    'add-on' => 'Extra',
+    'services-workshops' => 'Ateliers de science citoyenne',
+    'services-data-analysis' => 'Analyse personnalisée des données',
+    'services-custom-dev' => 'Développement sur mesure',
+    'services-embedded' => 'Site web intégré',
+
+
+    // table buttons
+    'contact-schedule-talk' => 'Organisez une réunion',
+
+];
---
+++ lang/nl/pricing.php
@@ -0,0 +1,115 @@
+<?php
+
+return [
+
+    'title' => 'Prijzen',
+    'subtitle' => 'Maak het mogelijk om als burger of als professional mobiliteitsprojecten aan te sturen.',
+
+    // ------------------------------------------------------
+    // ------------------- block #1
+    // ------------------------------------------------------
+    'title-block-1' => 'Overzicht',
+
+    'pack-individual-title' => 'Individueel',
+    'pack-individual-subtitle' => 'Voor het observeren van één locatie',
+    'pack-individual-number' => '1 toestel',
+
+    'pack-standard-title' => 'Standaard',
+    'pack-standard-subtitle' => 'Krachtige tools voor het beheren van een lokaal netwerk',
+    'pack-standard-number' => 'Tot 50 toestellen',
+
+    'pack-enhanced-title' => 'Enhanced',
+    'pack-enhanced-subtitle' => 'Bespaar tijd met premium analytische, rapportering en management tools voor uitgebreidere projecten',
+    'pack-enhanced-number' => 'Tot 50 toestellen',
+
+    'pack-custom-title' => 'Custom',
+    'pack-custom-subtitle' => 'Oplossingen voor de specifieke eisen van grotere projecten',
+    'pack-custom-number' => '50+ toestellen',
+
+    'per-device' => 'Per toestel',
+    'incl-vat' => '(incl BTW)',
+    'excl-vat' => '(ex BTW)',
+    'one-time-hardware' => 'Eenmalige hardware kost',
+    'month' => 'maand',
+    'subscription' => 'Abonnement',
+    'network-subscription' => 'Netwerkabonnement, jaarlijks gefactureerd',
+    'tailer-made' => 'Oplossingen op maat',
+    'free' => 'Gratis',
+
+    // buttons
+    'buy-a-telraam' => 'Koop een Telraam',
+    'get-started' => 'Start',
+    'get-in-touch' => 'Contact opnemen',
+
+    // includes
+    'include-title' => 'Inclusief:',
+    'individual-include-1' => 'Persoonlijk dashboard',
+    'individual-include-2' => 'Basis API toegang',
+    'individual-include-3' => 'Email support',
+
+    'standard-include-1' => 'Alle kenmerken van Individueel',
+    'standard-include-2' => 'Netwerk dashboard',
+    'standard-include-3' => 'Prioriteit support',
+
+    'enhanced-include-1' => 'Alle kenmerken van Standaard',
+    'enhanced-include-2' => 'Premium analytische tools',
+    'enhanced-include-3' => 'Premium rapportering',
+    'enhanced-include-4' => 'Beheer meerdere trajecten',
+
+    'custom-include-1' => 'Alle kenmerken van Enhanced',
+    'custom-include-2' => 'Branding op maat',
+    'custom-include-3' => 'Project ondersteuning',
+
+    // ------------------------------------------------------
+    // ------------------- block #2
+    // ------------------------------------------------------
+    'title-block-2' => 'Welke functionaliteiten krijg je in elk pakket?',
+
+    'pricing-per-device' => 'Prijs per toestel',
+    'subscription-software' => 'Abonnement op software',
+    'one-time-hardware-S2' => 'Eenmalige hardwarekost (Telraam S2)',
+
+    'usage' => 'Gebruik',
+    'who-is-this-for' =>  'Voor wie is dit?',
+    'individual-citizens' => 'Individuele burgers',
+    'professionals-campaigners' => 'Professionals en actievoerders',
+    'professionals' =>  'Professionals',
+
+    'features' => 'Kenmerken',
+    'personal-dashboard' => 'Persoonlijk dashboard',
+    'network-dashboard' => 'Netwerk dashboard',
+    'network-dashboard-feature-1'  => 'Manage netwerk settings',
+    'network-dashboard-feature-2' => 'Selectie van kandidaten',
+    'network-dashboard-feature-3' => 'Geautomatiseerde emails ',
+    'network-dashboard-feature-4' => 'Basis analytische tools',
+    'network-dashboard-feature-5' => 'Geavanceerde analytische tools',
+    'network-dashboard-feature-6' => 'Branding opties',
+    'network-dashboard-feature-7' => 'Meerdere trajecten',
+
+    'api-access' => 'API toegang',
+    'api-for-one-hour' => 'Basic API toegang voor 1u data',
+    'api-15min' => 'Geavanceerd API toegang: 15min data, typische verkeersdata, straatrapport',
+    'api-custom'  => 'Custom: API-opties op aanvraag, aangepaste tariefgrenzen',
+
+    'support' => 'Support',
+    'support-faq' => 'FAQ support artikels',
+    'support-tt' => 'Peer-to-peer ondersteuning via Telraam Talks',
+    'support-mail-low' => 'Mail support<br> (antwoord binnnen 3-5 werkdagen)',
+    'support-mail-high' => 'Priority mail support <br>(antwoord binnen 1 werkdag)',
+    'support-training' => 'Trainingsessies<br><i>“hoe beheer ik mijn netwerk"</i>',
+    'support-training-2h' => '2 x 1u (1-op-1)',
+    'support-training-4h' => '4 x 1u (1-op-1)',
+    'support-training-on-request' => 'Op aanvraag',
+
+    'services' => 'Diensten',
+    'add-on' => 'Extra',
+    'services-workshops' => 'Burgerwetenschap workshops',
+    'services-data-analysis' => 'Aangepaste data-analyse',
+    'services-custom-dev' => 'Development op maat',
+    'services-embedded' => 'Geïntegreerde website',
+
+
+    // table buttons
+    'contact-schedule-talk' => 'Plan een gesprek in',
+
+];
