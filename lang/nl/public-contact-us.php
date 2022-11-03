44f80b0703655a8356e71af67c8142fe013a48c0
elkeverhoeven <elke.verhoeven@telraam.net>
2022-11-03 11:55:46 +0100

    contact us NL + FR

---
+++ lang/nl/public-contact-us.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'title'       => 'Contacteer ons',
+    
+    'intro'       => 'Bedankt voor je interesse in Telraam.',
+
+    'text-above-form'       => 'Selecteer waarom je ons contacteer en vul dan het formulier in. Je bericht zal geforward worden naar ons team en we zullen je zo snel mogelijk antwoorden.',
+
+];
