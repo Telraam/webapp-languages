260398015bc71493345823cba1fff338e09a4cd2
elkeverhoeven <elke.verhoeven@telraam.net>
2022-11-03 11:51:18 +0100

    S2- Aanpassing NL + version FR

--- lang/nl/telraamV2.php
+++ lang/nl/telraamV2.php
@@ -4,7 +4,7 @@ return [
 
     'title'           => 'Telraam S2',
 
-    'subtitle'          => 'Onze nieuwe sensor - Beschikbaar vanaf maart 2023<br>Subtiel, slim, and doeltreffend<br>Moeiteloos verkeer tellen in elke straat',
+    'subtitle'          => 'Onze nieuwe sensor - Verwacht in maart 2023<br>Subtiel, slim, and doeltreffend<br>Moeiteloos verkeer tellen in elke straat',
 
     'section1-block1-text'        => '<p><strong>De ideale oplossing om gedetailleerde informatie over het verkeer te verzamelen</strong> op iedere straat, zowel voor particulieren en campagnevoerders als voor lokale overheden en mobiliteitsprofessionals in SMART-steden. </p>
     <p><strong>Volg meerdere modaliteiten tegelijk</strong> met de S2 om het effect te zien van eventuele ingrepen of veranderingen in lokale gewoonten.</p>',
@@ -13,12 +13,12 @@ return [
     'section-features-title'       => 'Kenmerken',
 
     'section-features-block1-title'  => 'Multimodaal',
-    'section-features-block1-subtitle'     => 'De straat, niet enkel voor auto\'s',
-    'section-features-block1-body'     => '<p>THet Telraam S2-apparaat telt, categoriseert en geeft de drukte op straat weer: het aantal auto\'s, fietsen, voetgangers, bussen, vrachtwagens en meer.</p><p> Krijg een nauwkeurig beeld van wie de straat gebruikt, ontdek patronen en anomalieën, en start zinvolle datagestuurde gesprekken over lokale verkeers- en mobiliteitsplannen.',
+    'section-features-block1-subtitle'     => 'Straten zijn er niet enkel voor auto\'s',
+    'section-features-block1-body'     => '<p>Het Telraam S2-apparaat telt, categoriseert en geeft de drukte op straat weer: het aantal auto\'s, fietsen, voetgangers, bussen, vrachtwagens en meer.</p><p> Krijg een nauwkeurig beeld van wie de straat gebruikt, ontdek patronen en anomalieën en start zinvolle datagestuurde gesprekken over lokale verkeers- en mobiliteitsplannen.',
 
     'section-features-block2-title'  => 'Speciaal voor ons gebouwd',
     'section-features-block2-subtitle'     => 'Een toestel voor volledige autonomie',
-    'section-features-block2-body'     => '<p>Telraam S2 is volledig nieuw ontworpen en gebouwd met het specifieke doel om verkeer te tellen. Het is robuust en betrouwbaar en werkt volledig autonoom dankzij de ingebouwde mobiele dataverbinding en de tools voor remote toestelbeheer.</p>
+    'section-features-block2-body'     => '<p>Telraam S2 is volledig nieuw ontworpen en gebouwd met het specifieke doel om verkeer te tellen. Het is robuust, betrouwbaar en werkt volledig autonoom dankzij de ingebouwde mobiele dataverbinding en de tools voor toestelbeheer op afstand.</p>
         <p><ul class="jump-in">
         <li>Op maat gemaakte PCB met AI-chip</li>
         <li>Geïntegreerde mobiele dataverbinding</li>
@@ -30,20 +30,19 @@ return [
 
     'section-features-block3-title' => 'Simpel',
     'section-features-block3-subtitle'     => 'Vijf minuten installation',
-    'section-features-block3-body' => '<p>Telraam S2, plug en play</p>
-        <p>Na een eenvoudige set-up werkt het toestel, verstuurt het gegevens zonder dat wifi nodig is, en kan het op afstand worden geüpdated.</p>
+    'section-features-block3-body' => '<p>Na een eenvoudige set-up werkt het toestel, verstuurt het gegevens zonder dat wifi nodig is, en kan het op afstand worden geüpdated.</p>
         <p>Plaats je Telraam S2 aan de binnenkant van je raam met de meegeleverde zelfklevende houder, sluit de USB-C-kabel aan, scan een code met je smartphone* en voltooi de registratie op je scherm. Geen gedoe met wifi-wachtwoorden, of het aanpassen en regelen van de camerahoek. Binnen 15 minuten na registratie staat je Telraam S2 live op de kaart!</p>
-        <p>Het LCD-scherm helpt niet alleen bij de installatie, maar geeft ook een overzicht van het actuele verkeer van de dag en het afgelopen kwartier.</p>
+        <p>Het LCD-scherm helpt bovendien niet alleen bij de installatie, maar geeft ook een overzicht van het actuele verkeer van de dag en het afgelopen kwartier.</p>
         <p class=tiny>* Scan de meegeleverde QR-code of voer een code in in eender welke browser</p>',
 
     'section-features-block4-title' => 'Elegant',
-    'section-features-block4-subtitle'     => 'Nieuw ontwerp met functionaliteit en gebruiksvriendelijkheid als leidraad',
+    'section-features-block4-subtitle'     => 'Functionaliteit en gebruiksvriendelijkheid als leidraad',
     'section-features-block4-body' => '<p>Een ontwerp dat in je dagelijks leven, omgeving en meer specifiek op het uitzicht op je raam goed staat.</p>
-        <p>Het apparaat wordt gevoed via een USB-C-poort, heeft één knop voor gebruikersinteractie en wordt geleverd met een LCD-kleurenscherm. Het scherm kan volledig worden uitgeschakeld, wat energie bespaart en voorkomt dat er mogelijk ongewenst licht van uw Telraam S2 komt - of het nu in een woonkamer of slaapkamer is.</p>
+        <p>Het apparaat wordt gevoed via een USB-C-poort, heeft één knop voor gebruikersinteractie en wordt geleverd met een LCD-kleurenscherm. Het scherm kan volledig worden uitgeschakeld, wat energie bespaart en voorkomt dat er mogelijk ongewenst licht van je Telraam S2 komt, of het nu in een woonkamer of slaapkamer is.</p>
         <p>Telraam S2 wordt verkocht in 100% recyclebare papieren verpakkingen, zonder plastic afval.</p>',
 
     'section-features-block5-title' => 'Nauwkeurigheid',
-    'section-features-block5-subtitle'     => 'AI voor nauwkeurigheid, precisie, nachttellingen en anonieme gegevens',
+    'section-features-block5-subtitle'     => 'AI voor nauwkeurigheid, precisie, nachttellingen en anonieme data',
     'section-features-block5-body' => '<p>Telraam S2 gebruikt een volledig nieuwe telmethode in vergelijking met onze eerste Raspberry Pi-gebaseerd toestel, dit met behulp van kunstmatige intelligentie (AI) en neurale netwerken. Dit resulteert in een verbeterde precisie en nauwkeurigheid, en ook bij donkere omstandigheden voor gemotoriseerde voertuigen.</p>',
 
     'section-features-block6-title' => 'Privacy',
@@ -52,10 +51,10 @@ return [
 
     'section-features-block7-title' => 'Tel mee!',
     'section-features-block7-subtitle'     => 'Een oplossing voor elke straat',
-    'section-features-block7-body' => '<p>Of je nu een particulier bent die een enkel toestel nodig heeft voor uw straat, of een professional die een heel netwerk in uw stad wil opzetten, Telraam is er om u te helpen. Je kan ook kiezen voor hulpmiddelen voor het bouwen en beheren van lokale netwerken, of het nu gaat om een kleine wijk of een metropool. Bekijk hieronder de optie die het beste bij jou past om aan de slag te gaan.</p>',
+    'section-features-block7-body' => '<p>Of je nu een particulier bent die een enkel toestel nodig heeft voor in je straat, of een professional die een heel netwerk in je stad wil opzetten, Telraam is er om je te helpen. Je kan ook kiezen voor hulpmiddelen voor het bouwen en beheren van lokale netwerken, of het nu gaat om een kleine wijk of een metropool. Bekijk hieronder de optie die het beste bij jou past om aan de slag te gaan.</p>',
 
     // tech table
-    'spec-table-title' => 'Technische specificatiets',
+    'spec-table-title' => 'Technische specificaties',
 
     'spec-table-row-1-subject'     => 'AI',
     'spec-table-row-1-details' => 'K210 edge computing chip',
