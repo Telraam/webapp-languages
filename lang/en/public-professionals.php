63e1a64d718c38fc2149200d9735df6722d6f1b9
telraamtalks <116150500+telraamtalks@users.noreply.github.com>
2022-11-08 12:29:21 +0000

    updated EN text
    tweaks to translations

--- lang/en/public-professionals.php
+++ lang/en/public-professionals.php
@@ -2,18 +2,15 @@
 
 return [
     'title' => 'Professionals',
-    'subtitle' => 'Telraam at municipal or city level thanks to a finely meshed network of Telraam-devices by citizens',
+    'subtitle' => 'Deploy Telraam at local, municipal or city level with a finely meshed network of Telraam devices hosted by citizens',
 
     // quotes
     'testimonials' => 'Testimonials',
 
-    'quote1' => 'The city council of Kortrijk wants to take more data-driven decisions. We already do this with our own
-    measuring equipment, but with innovative techniques, such as Telraam, we can measure for a long time in several locations at the same time.',
+    'quote1' => 'The city council of Kortrijk wants to take more data-driven decisions. We already do this with our own measuring equipment, but with innovative techniques, such as Telraam, we can measure for a long time in several locations at the same time.',
     'quote1-author' => 'Lotte Demeestere, mobility expert for the city Kortrijk',
 
-    'quote2' => 'By analysing the V85 standard, we get an indication of the best time to use the device for speed checks.
-    speed checks is best used to achieve maximum effect with a view to changing the behaviour of road users.
-    of road users.',
+    'quote2' => 'By analysing the V85 standard, we get an indication of the best time to use the device for speed checks. Speed checks is best used to achieve maximum effect with a view to changing the behaviour of road users.',
     'quote2-author' => 'David Tailleur, teamcoach neighbourhood Police Lubbeek ',
 
     'quote3' => 'Telraam provides not only insights through data but also understanding through interaction with residents, local and provincial government.',
@@ -24,68 +21,45 @@ return [
 
     // main content
     'section1-block1-title' => 'Reliable measurement with citizens',
-    'section1-block1-text' => 'Traffic counts are essential for a sound mobility policy. Countings that are multimodal, and
-thus register cars, heavy traffic as well as cyclists and pedestrians, are the base of any traffic engineering study. Classic traffic counts
- are limited today: both in time and in counting modes. In addition, they are often expensive and not very transparent.<br>
+    'section1-block1-text' => 'Traffic counts are essential for a sound mobility policy. Counts that are multimodal, and register cars and heavy vehicles as well as cyclists and pedestrians, are the base of any traffic engineering study. Classic traffic counts are limited today: both in time and in counting modes. In addition, they are often expensive and not very transparent.<br><br> 
+    New technology can provide innovative measurement methods without costing a fortune. Telraam has developed high-tech and reliable measuring equipment that quantifies and categorises traffic in a finely-meshed, continuous, multimodal way and in conjunction with citizens.
+ Telraam provides the research effort and the appropriate technology and data services, and the citizens count and provide local insight to support it. <br>
  <br>
-New technologies provide innovative measurement methods. Telraam has developed high-tech and reliable measuring equipment that measures
- and analyse traffic in a finely-meshed, continuous, multimodal way and in conjunction with citizens.
- Telraam provides the right research question and the appropriate research technology, and the citizens count and think along with it. <br>
- <br>
-For this purpose, we developed a Telraam trajectory that we offer to local governments. The local government chooses a programme of, for example, 8 months, citizens can apply and start counting after screening and selection.
-Data analysis and support is provided during the process. Telraam takes care of everything and provides support from start to finish.<br>',
+We have developed a Telraam solution for local governments and authorities as well as campaign groups. The organisation chooses a programme running for several months (usually 6-12+), then engages citizens to apply, and after screening and selection can start counting.
+Data analysis and support is provided by the Telraam team throughout the process, from start to finish.<br>',
 
     'section2-small-title' => 'How does it work?',
     'section2-block1-title' => 'Citizens',
-    'section2-block1-text' => 'Telraam actively engages with citizens to deliver traffic counts. Residents are motivated to
-    discover how much and what kind of traffic passes along through their street and so have a direct personal
-    involvement in local mobility policy. In this way, highly relevant traffic data is collected in a participatory
-     manner that can underpin local policy decisions. <br>
+    'section2-block1-text' => 'Telraam provides a means to actively engage with citizens to deliver traffic counts. Residents are already motivated to discover how much and what kind of traffic passes along through their street and so have a direct personal involvement in local mobility policy. In this way, highly relevant traffic data is collected in a participatory manner that can underpin local policy decisions. <br>
 <br>
-Citizens are guided throughout the entire Telraam project. They learn how to set-up their own fully automatic traffic counter
- and how to interpret the data and even to communicate about it. They can then collaborate with others, neighbours, local
- campaign groups, citizen movements and other counters across the world.<br>
+    Citizens who apply and are selected, are guided throughout the entire Telraam project. They learn how to set-up their own fully automatic traffic counter and how to interpret the data and even to communicate about it. They can then collaborate with others, neighbours, local campaign groups, citizen movements and other counters across the world.<br>
 <br>
-This way, participating citizens become citizen scientists that make constructive, objective and data-driven contributions to
-local mobility policy that is also engaging and relevant.',
+    In this way, participating citizens become citizen scientists that make constructive, objective and data-driven contributions to local mobility policy that is also engaging and relevant.',
 
     'section2-block2-title' => 'Data for efficient traffic plans',
-    'section2-block2-text' => 'The collected counting data are a valuable basis to be used in traffic and
-    mobility plans of a city or municipality. <br>
-    <br>Telraam data can also be used to measure the concrete impact of a planned mobility
-measure or intervention in your municipality. In this way, the collected measurement data supports
-    policy measures.<br><br>
-    The data can also be used by researchers, residents and interested parties to support a position or choice',
+    'section2-block2-text' => 'The count data collected are a valuable basis for  traffic and mobility planning and decision-making in a city or municipality. <br>
+    <br>Telraam data can also be used to measure the concrete impact of a planned mobility measure or intervention, supporting and validating policy measures.<br><br>
+    The data can also be used by researchers, residents and interested parties to support a position or choice, helping to make local discussions more constructive.',
 
     'section2-block3-title' => 'Telraam workshops',
-    'section2-block3-text' => 'After several months of counting, the participants come together again for a Telraam data workshop.<br><br>
-    The traffic data collected by the Telraam devices in the counting network are explained.
-    In this workshop we make analyses in a participative way, draw conclusions and formulate proposals, together with citizens. In
-    In consultation with the local government, certain accents can be set or specific measures and traffic situations can be included.<br>
+    'section2-block3-text' => 'After several months of counting, participants are invited to come together again for a Telraam data workshop.<br><br>
+    The traffic data collected by the Telraam devices in the network are explained. In these interactive workshops we conduct collaborative analysis and interpretation, drawing conclusions and formulating proposals in partnership with citizens. In consultation with the network manager and client, certain subjects or solutions can be included or specific measures and traffic situations can be explored.<br>
     <br>
-The data workshop focuses on using the unique knowledge of the local context of the participating citizens.
-After all, who knows their own street better than those who live there?',
+    The data workshops focus on capturing the unique knowledge and local context of the citizens who participate. After all, who knows what insights can be gathered from involving those who actually live there?',
 
     'section2-block4-title' => 'Open data',
     'section2-block4-text' => 'Open and transparent traffic data are essential for mobility policy.<br><br>
-The open data also offers the opportunity for other interested parties to look into the matter.
-Telraam provides the following tools for this:<br>
+    Open data also offers the opportunity for other interested parties to add value. Telraam provides the following tools for this:<br>
 <ul class="text-list">
-    <li>The collected traffic data is visible to everyone via the interactive map on the Telraam homepage.</li
-    <li>The professional Telraam network dashboard allows local authorities to monitor the Telraam-devices in their project area,
-     export the data and perform live analyses.
-    <li>Through the Telraam API, anyone can retrieve all traffic data directly, under an open data licence, for application
-    in their own database or existing dashboard. Telraam has already won the award for API of the Year in 2020.</li>
+    <li>The traffic data collected is made available to the public via the interactive map on the Telraam homepage.</li>
+    <li>The professional Telraam network dashboard allows local authorities to monitor the Telraam devices in their project area, export the data and perform live analysis.</li>
+    <li>Through the Telraam API, anyone can retrieve all traffic data directly, under an open data, non-commercial licence, for use in their own database or existing dashboard. Telraam won the award for "API of the Year" in 2020.</li>
     </ul>',
     'section2-block4-note' => 'Telraam, API of the year',
 
-    'section3-block1-title' => 'A Telraam trajectory in your city?',
-    'section3-block1-text' => 'A Telraam trajectory in which citizen science and the collection of objective multimodal traffic data track together,
-    offers a unique proposition. Due to the well-considered combination of the different building blocks in a Telraam project and our experience with dozens of Telraam projects for local governments, we have been able to
-    Telraam projects for local authorities, we have the expertise to develop a successful Telraam project for your city or municipality. <br>
-    <br>
-For an Telraam project tailored to your municipality and fitting the local policy context, we can put forward a suitable project proposal.
-If you would like more information or a quotation, please contact us at <a href="sales@telraam.net">sales@telraam.net</a>,
-we will be pleased to help you.'
+    'section3-block1-title' => 'A Telraam network in your city?',
+    'section3-block1-text' => 'A Telraam network in which citizen science conducts the collection of objective, multimodal traffic data is a unique proposition. Through the combination of these different building blocks in a Telraam project and our experience with dozens of Telraam projects for local authorities, we have built the expertise to develop a successful Telraam project for your city or municipality. <br><br>
+    Talk to us about putting together a Telraam project tailored to your municipality and fitting the local policy context. <br><br>
+    If you would like more information or a quotation, visit our <a href="https://staging.telraam.net/en/contact-us">Contact Us page</a>, and we will be pleased to help you.'
 
 ];
