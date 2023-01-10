ed9851228b45b0024e208bbda272a0df1bae6203
Dave Driesmans <dave@waanz.in>
2023-01-10 15:25:43 +0100

    Setup S2

---
+++ lang/en/public-setup.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    
+    'title' => 'Telraam S2',
+
+    'subtitle' => 'Gather detailed information on traffic on any street',
+    'description' => 'Congratulations on being an S2 host. In the next steps we explain how to instal the device',
+    
+   'current-installation-title' => 'Current installation of this device',
+    'is-counting' => 'Is counting',
+    'counted-before' => 'Counted before',
+    'no-counting-yet' => 'No counting done yet',
+    
+    'view-the-measurements' => 'View the measurements',
+    
+    
+    'ready-to-install' => 'You are ready to install the Telraam device on your location?',
+    'ready-to-install-new-owner' => 'You want to install the Telraam device on a new location or it has a new owner?'
+    
+    'log-in' => 'Log in',
+    'or' => 'or',
+    'create-an-account' => 'Create an account',
+    
+    'want-to-install-with' => 'Great! I want to install this device with'
+    'start-setup' => 'Start the setup'
+    'want-to-install-with-another-user' => 'I want to install it with another user',
+    
+    'i-could-use' => 'Oh no, I could use',
+   'support' => 'support',
+    'to-get-up' => 'to get this device up and running again.'
+];
