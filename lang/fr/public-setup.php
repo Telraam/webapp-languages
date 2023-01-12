f71e4b9b8eb9021d1cd81a8db3372196b4ac1bf8
elkeverhoeven <elke.verhoeven@telraam.net>
2023-01-12 10:19:49 +0100

    update setup v2

---
+++ lang/fr/public-setup.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    
+    'title' => 'Telraam S2',
+
+    'subtitle' => 'Rassemblez des informations détaillées sur la circulation sur n\'importe quelle rue',
+    'description' => 'Félécitations, vous êtes un hôte S2! Dans les prochaines étapes, nous expliquons comment installer l\'appareil',
+    
+   'current-installation-title' => 'Installation actuelle de cet appareil',
+    'is-counting' => 'Compte',
+    'counted-before' => 'Compté avant',
+    'no-counting-yet' => 'Aucun comptage effectué déjà',
+    
+    'view-the-measurements' => 'Voir les mesures',
+    
+    'start-installation-title' => 'Démarrer l\'installation', //  install with device never used
+    'start-installation-not-new-title' => 'Plus...',
+
+    'ready-to-install' => 'Êtes-vous prêt à installer l\'appareil Telraam à votre emplacement?',
+    'ready-to-install-new-owner' => 'Voulez-vous installer l\'appareil Telraam à un nouvel emplacement ou il a un nouveau propriétaire?',
+    
+    'log-in' => 'Connectez-vous',
+    'or' => 'ou',
+    'create-an-account' => 'Créez un compte',
+    
+    'want-to-install-with' => 'Génial! Je veux installer cet appareil avec',
+    'start-setup' => 'Démarrer la configuration',
+    'want-to-install-with-another-user' => 'Je veux l\'installer avec un autre utilisateur',
+    
+    'i-could-use' => 'Oh non, je pourrais utiliser',
+   'support' => 'soutien',
+    'to-get-up' => 'pour remettre cet appareil en marche.'
+];
