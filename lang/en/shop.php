38475e7b7c72359a66c3ee19743ab71c5e890125
Dave Driesmans <dave@waanz.in>
2023-02-03 10:24:41 +0100

    ship vs delivery correction

--- lang/en/shop.php
+++ lang/en/shop.php
@@ -9,5 +9,5 @@ We offer two packages right away in the shop. <br>
 One package includes 10 Telraam S2 devices with network dashboard access for 1 year, another is a starter package for 6 months with 4 devices.<br><br>
     Do not hesitate to get in <a href="https://telraam.net/en/contact-us">touch with us</a> for quotes, information or questions. We love to hear from you',
 
-    'intro-individual' => 'As an individual you can buy one device for your own street at a discounted rate. Estimated delivery is March 15, 2023'
+    'intro-individual' => 'As an individual you can buy one device for your own street at a discounted rate. Estimated ship date is March 15, 2023'
 ];
--- lang/fr/shop.php
+++ lang/fr/shop.php
@@ -8,5 +8,5 @@ Nous vous proposons deux forfaits tout de suite en boutique. <br>
 Un forfait comprend 10 appareils Telraam S2 avec accès au tableau de bord réseau pendant 1 an, un autre est un forfait de démarrage pour 6 mois avec 4 appareils.<br><br>
      N\'hésitez pas à entrer en <a href=https://telraam.net/fr/contact-us>nous contacter</a> pour des devis, des informations ou des questions. Nous aimons avoir de vos nouvelles',
 
-     'intro-individual' => 'En tant que particulier, vous pouvez acheter un appareil pour votre propre rue à un tarif réduit. La livraison estimée est le 15 mars 2023'
+     'intro-individual' => 'En tant que particulier, vous pouvez acheter un appareil pour votre propre rue à un tarif réduit. La date d\'expédition estimée est le 15 mars 2023'
 ];
