7465f3ae2422c44cda69d93c82f17633b4d7f696
telraamtalks <116150500+telraamtalks@users.noreply.github.com>
2023-02-03 10:55:05 +0000

    added estimated date

--- lang/en/shop.php
+++ lang/en/shop.php
@@ -11,7 +11,8 @@ return [
 <li>a single device with 12 months of connection and updates</li>
 <li>a starter package to gather data for 12 months with 4 devices</li>
 <li>a complete package with 10 Telraam S2 devices with network dashboard access for 1 year</li></ul>
-<p>Do not hesitate to get in <a href="https://telraam.net/en/contact-us">touch with us</a> for quotes, information or questions. We love to hear from you</p>',
+<p>Do not hesitate to get in <a href="https://telraam.net/en/contact-us">touch with us</a> for quotes, information or questions. We love to hear from you</p>
+<p>Estimated delivery date: 15 March 2023</p>',
 
     'intro-individual' => 'As an individual you can buy a single device at a discounted rate for personal use to track your own street.'
 ];
