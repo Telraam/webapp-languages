7845e632777d741ef50b326d1b1e4240ec26ec37
Dave Driesmans <dave@waanz.in>
2023-02-03 13:46:41 +0100

    Shop updates

--- lang/en/shop.php
+++ lang/en/shop.php
@@ -5,13 +5,15 @@ return [
     'title'           => 'Shop',
 
     'intro-professional'          => '<p>Get your network up and running today.</p>
-     <p><a href="https://telraam.net/en/contact-us">Get in touch</a> to talk to one of the team so we can help you build the right network for you.</p>>
-<p>To get you counting QUICKLY, we currently offer some ready-made packages in the shop. </p>
-<ul>
+     <p><a href="https://telraam.net/en/contact-us">Get in touch</a> to talk to one of the team so we can help you build the right network for you.</p>
+<p>To get you counting QUICKLY, we currently offer some ready-made packages in the shop. </p>',
+
+    'more-professional'  => '<ul>
 <li>a single device with 12 months of connection and updates </li>
 <li>a starter package to gather data for 1 year with 4 devices</li>
-<li>a complete package with 10 Telraam S2 devices with network dashboard access for 1 year</li></ul>
-<p>Do not hesitate to get in <a href="https://telraam.net/en/contact-us">touch with us</a> for quotes, information or questions. We love to hear from you</p>
+<li>a complete package with 10 Telraam S2 devices with network dashboard access for 1 year</li></ul>',
+
+    'outro-professional'  => '<p>Do not hesitate to get in <a href="https://telraam.net/en/contact-us">touch with us</a> for quotes, information or questions. We love to hear from you</p>
 <p>Estimated shipping date: 15 March 2023</p>',
 
     'intro-individual' => 'As an individual you can buy a single device at a discounted rate for personal use to track your own street. Estimated shipping date is March 15, 2023'
--- lang/fr/shop.php
+++ lang/fr/shop.php
@@ -5,12 +5,15 @@ return [
 
      'intro-professional' => 'Obtenez un réseau simple opérationnel dès aujourd\'hui<br>
  <p><a href="https://telraam.net/en/contact-us">Contactez-nous</a> pour parler à l\'un des membres de l\'équipe afin que nous puissions vous aider à construire le réseau qui vous convient.</p>>
-<p>Pour vous permettre de compter RAPIDEMENT, nous proposons actuellement des formules toutes prêtes dans la boutique. </p>
+<p>Pour vous permettre de compter RAPIDEMENT, nous proposons actuellement des formules toutes prêtes dans la boutique. </p>',
+
+    'more-professional' => '
 <ul>
 <li>un seul appareil avec 12 mois de connexion et de mises à jour </li>
 <li>un pack de démarrage pour recueillir des données pendant 1 an avec 4 appareils</li>
-<li>un pack complet avec 10 appareils Telraam S2 avec accès au tableau de bord du réseau pendant 1 an</li></ul>
-     N\'hésitez pas à entrer en <a href=https://telraam.net/fr/contact-us>nous contacter</a> pour des devis, des informations ou des questions. Nous aimons avoir de vos nouvelles',
+<li>un pack complet avec 10 appareils Telraam S2 avec accès au tableau de bord du réseau pendant 1 an</li></ul>',
+
+    'outro-professional'  => 'N\'hésitez pas à entrer en <a href=https://telraam.net/fr/contact-us>nous contacter</a> pour des devis, des informations ou des questions. Nous aimons avoir de vos nouvelles',
      '<p>La date d\'expédition estimée: 15 mars 2023</p>',
 
      'intro-individual' => 'En tant que particulier, vous pouvez acheter un appareil pour votre propre rue à un tarif réduit. La date d\'expédition estimée est le 15 mars 2023'
