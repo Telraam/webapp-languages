e5b514dc882375226aa2fd75b288eb0f42ff75b1
Péter I. Pápics <papics@gmail.com>
2023-02-20 17:09:55 +0100

    Update public-new-what-is-telraam.php
    Minor fixes (word logic, missing commas, quotation marks, and a period), some clarifications, removed a few redundant repetitions, added link to Start counting. I hope I did not screw up the php code anywhere.

--- lang/en/public-new-what-is-telraam.php
+++ lang/en/public-new-what-is-telraam.php
@@ -3,38 +3,38 @@
 return [
     'title' => 'Your window on local traffic',
     'sub-title' => 'Traffic data for better streets.<br>
-Get high quality, real-time and coninuous data, in collaboration with local citizens.',
+Get high quality, coninuous and real-time data, in collaboration with local citizens.',
 
   'section1-block1-title' => 'What is Telraam?',
     'section1-block1-text' => 'Telraam is your citizen-powered solution for collecting multi-modal traffic data with a purpose-built, affordable, and user-friendly device.<br>
 <br>
-Our Telraam sensor continuously monitors a street from a citizen’s window, providing crucial data on various modes of transport, including motorised vehicles, cyclists, pedestrians, and more. Telraam networks also create the opportunity for dialogue between traffic planners, local authorities and their most affected communities: the citizens who live on, and use, these streets, by turning traffic counting into an open and accessible citizen science project.<br><br>
-Whether you are a resident, a campaigner, or work for a public / private organisation or political authority, a Telraam citizen science project delivers reliable data to help you achieve your goals.
+Our Telraam sensor continuously monitors a street from a citizen’s window, providing crucial data on various modes of transport, including motorised vehicles, cyclists, pedestrians, and more. Telraam networks also create the opportunity for dialogue between traffic planners, local authorities and their most affected communities: the citizens who live on - and use - these streets, by turning traffic counting into an open and accessible citizen science project.<br><br>
+Whether you are a local resident, a campaigner, an employee of a public or private organisation, or work for a government agency, a Telraam project delivers reliable data to help you achieve your goals.
 ',
 
 
 
     'section2-block1-title' => 'Inform the debate',
-    'section2-block1-text' => 'Rat runs. School runs. Air pollution. Road safety. Active Travel. Complex issues require traffic data to enhance our understanding and find better solutions.<br>
+    'section2-block1-text' => 'Rat running. School runs. Road safety. Air pollution. Active Travel. Complex issues require quantitative traffic data to enhance our understanding and to find better solutions.<br>
     <br>
-    Gathering data to get a full picture of transport, leisure and social activity for streets is often impossible or very expensive with traditional tools like pneumatic tubes or manual counts. <br>
+    Gathering data to get a full picture of transport, leisure, and social activity for a newtowrk of streets is often impossible or very expensive with traditional tools like pneumatic tubes or manual counts. <br>
 <br>
-Telraam sensors and networks provide a solution for citizens and their local representatives',
+Telraam sensors and networks provide a solution for citizens and their local representatives.',
 
 
     'section2-block2-title' => 'Telraam is your citizen science solution',
     'section2-block2-text' => 'Telraam creates custom networks of sensors that leverage the knowledge
     and access of local residents, using artificial intelligence (AI) technology to deliver consistent, timely, quality data for more efficient and effective infrastructure, traffic flow and traffic management plans.<br>
     <br>
-Involving citizens to produce Open Data brings researchers and policy makers into contact with residents, creating the potential for interaction, dialogue, co-creation and other forms of participation.',
+Involving citizens and publishing open data brings researchers and policy makers into contact with local residents, creating the potential for interaction, dialogue, co-creation, and other forms of participation.',
 
-    'section2-cta' => 'Speak to us today about a Telraam count in your street',
+    'section2-cta' => 'Speak with us today about a Telraam count in your street',
 
 // testimonials
-    'quote1' => 'By analysing the V85 standard, we get an indication of the best time to use the device for speed checks. Speed checks is best used to achieve maximum effect with a view to changing the behaviour of road users.',
+    'quote1' => 'By analysing the V85 standard, we get an indication of the best times for performing speed checks. These checks are used to achieve a change in the behaviour of the road users.',
     'quote1-author' => 'David Tailleur, teamcoach neighbourhood Police Lubbeek ',
 
-    'quote2' => 'Telraam provides not only insights through data but also understanding through interaction with residents, local and provincial government.',
+    'quote2' => 'Telraam provides not only insights through data but also understanding through interaction with residents, and the local and provincial governments.',
     'quote2-author' => 'Eric van Dijk, Beleidsmedewerker smart mobility, Province of Utrecht',
 
 
@@ -44,20 +44,18 @@ Involving citizens to produce Open Data brings researchers and policy makers int
 <br>
 Telraam S2 uses specially-trained AI and a proprietary tracking algorithm to detect, classify, and count road users with high precision in a wide range of typical street environments. Citizens place a Telraam sensor in their window, and the open data that is colleected can be shared with local authorities and policy makers.<br>
 <br>
-Telraam devices can be set up by citizens on their individual streets, by local campaign groups or government authorities who fund citizen networks in order to gather local traffic data.<br>
+Telraam devices can be set up by citizens on their individual streets, or by local campaign groups and government authorities who fund citizen networks in order to gather local traffic data.<br>
  <br>
 <a href="/en/S2">Read more about the S2 device</a>',
 
-    'section3-block2-title' => 'What does a Telraam device do?',
+    'section3-block2-title' => 'What does a Telraam S2 device do?',
     'section3-block2-text' => 'The Telraam S2 is a custom device that sits in your window.
 Once connected, you don’t need to do anything, just watch the counts as they happen.<br>
 <br>
-The S2 has a low resolution camera (for privacy), an AI chip for identifying and categorising street users by mode*, and an Internet-of-Things (IoT) connection that relays data back to the servers. The device simply needs to be powered and mounted on the inside of an upper-floor window with a view over the street. <br>
-<br>
-<div class="tiny"><sup>*</sup> the S2 can separately identify cars, vans, trucks, buses, pedestrians, bikes and motorcycles.</div>',
+The Telraam S2 has a low resolution camera (for privacy), an AI chip for detecting and categorising street users, and a built in mobile data connection that relays aggregated, anonymous data to the Telraam servers. The device simply needs to be powered and mounted on the inside of an upper-floor window with an unobstructed view over the street.',
 
     'section3-block3-title' => 'Make your view count',
-    'section3-block3-text' => 'Ready to start collecting your own data? Read our ‘Start Counting’ guide to see
+    'section3-block3-text' => 'Ready to start collecting your own data? Read our <a href="/en/self-measure">Start counting</a> guide to see
     how easy it is to get going, and how to set up your own counting network.<br>
 <br>
 In no time you will have a detailed insight report on all the activity on your street, neighbourhood or city.<br>
