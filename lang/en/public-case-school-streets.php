d77561fb7937cfda4f97299ea04ee28545ccd57a
Dave Driesmans <dave@waanz.in>
2023-04-19 11:00:11 +0200

    Case School street

--- lang/en/public-case-school-streets.php
+++ lang/en/public-case-school-streets.php
@@ -2,73 +2,129 @@
 
 return [
 
+    // general
+    'examples-title' => 'Examples of Telraam applications',
+    'location' => 'Location',
+    'number-of-devices' => 'Devices',
+    'challenges' => 'Challenges',
+    'link' => 'Link',
+
+
     'title' => 'Traffic counts for School Streets',
     'intro' => 'How does a Telraam traffic counting network help to secure and maintain a School Street in your area?',
 
+
+    'example-1-title' => 'Polivisu',
+    'example-1-body' => 'The Mechelen case tries to link data from ANPR-camera’s and/or other traffic measurements with a traffic model and the information from the GIPOD system.
+The Polivisu project was an example of a third party using the Telraam-API to build an own dashboard,
+specifically to monitor the impact of schoolstreets on the traffic on the school street and adjacent street.
+About Telraam devices were installed around the school area and data was collected continuously for 6-12 months,
+with some devices still currently active. Telraam helped by counting cyclists and pedestrians in a cost-efficient way while having a proper open api.',
+
+    'example-1-location' => 'Mechelen, Antwerpen',
+     'example-1-number' => '10-15 devices',
+    'example-1-challenges' => 'Api, focus on cyclists and pedestrians',
+    'example-1-link' => 'https://policyvisuals.eu/impact-of-temporary-road-blocks-by-road-works-or-for-a-safer-school-environment/',
+
+
     'section-1-title' => 'School street campaign goals',
-    'section-1-text' => '
-    <p>The goal of any local school street campaign is to make the experience of arriving and leaving the school safer, healthier and more
-     enjoyable for children, parents and local residents, usually by temporarily closing the street to cars and other vehicles, and opening
-      them to children to walk, cycle and be active.</p>
-<p>The definition from the UK School Streets Initiative <span class="tiny">[<a href="http://schoolstreets.org.uk/" target="_blank">&#128279;</a>]</span> is:
+    'section-1-text' => ' <p>In recent years the implementation of school streets has definitely gained momentum in various European countries. Pioneering with the
+    deployment of ‘Schoolstraten’ in the Netherlands and Belgium. With now close to 200 school streets permanently
+    deployed in the Flanders region. In recent years, also the ‘Rues aux écoles’ in France have gained traction,
+    with 168 currently implemented in Paris alone.
+    <span class="tiny"><a href="https://www.paris.fr/pages/57-nouvelles-rues-aux-ecoles-dans-paris-8197"><span class="heroicon heroicon-external-link"></span></a></span>
+    Also in the UK school streets have been successfully adopted, predominantly in some London boroughs. </p>
+
+
+<p class="focus">A school street, technically speaking, means temporarily closing down of the road(s) in the immediate surroundings of a school entrance for motorised traffic,
+prior to school start and after the school ending.</p>
+<p>By doing so, school streets directly aim to: </p>
+<ul class="jump-in">
+ <li>improve traffic safety for active travelling children </li>
+ <li>reduce the exposure to air pollution of school going children <a href="https://telraam.net/en/traffic-counts-for-clean-air"><span class="heroicon heroicon-external-link"></span></a> </li>
+ <li>promote walking and cycling to school</li>
+ </ul>
+
+<p>A study carried out in Flanders has demonstrated the positive effects of school streets for air and noise pollution, traffic safety and active travel.
+ <span class="tiny">(<a href="https://vito.be/en/news/healthier-school-thanks-school-street"><span class="heroicon heroicon-external-link"></span></a>)</span>
+
+<p><strong>How can Telraam sensors play a role in the school street implementation:</strong> </p>
+<ul class="jump-in">
+ <li>Telraam devices can monitor the traffic patterns in the school street, and observe possible long term behavioural changes in mobility patterns.  </li>
+ <li>A strategically deployed network of Telraam devices allows it to track possible spillover traffic in the surrounding streets.  </li>
+ <li>Engagement of neighbouring citizens with data collection allows to inform and gain citizen support for the school street implementation  </li>
+ <li>Engagement of children with data collection is a great learning opportunity  </li>
+ <li>Having detailed results on the benefits, and monitoring possible negative effects, are great communication assets for the local authority towards parents, neighbourhood and children. </li>
+</ul>
+',
+
+
+    'section-2-title' => 'Setting out a school street campaign ',
+    'section-2-text-col-1' => '<p>The goal of any local school street campaign is to make the experience of arriving and leaving the school safer, healthier and more enjoyable for children, parents and local residents, usually by temporarily closing the street to cars and other vehicles, and opening them to children to walk, cycle and be active.
+The definition from the UK School Streets Initiative [<a href="http://schoolstreets.org.uk"><span class="heroicon heroicon-external-link"></span></a>] is:
 
 <blockquote>
 “A School Street is a road outside a school with a temporary restriction on motorised traffic at school drop-off and pick-up times.
  The restriction applies to school traffic and through traffic. The result is a safer, healthier and pleasant environment for everyone.<br>
-<br>
-School Street schemes offer a proactive solution for school communities to tackle air pollution, poor health and road danger reduction.
-A School Street scheme will encourage a healthier lifestyle and active travel to school for families and lead to a better environment for everyone.”
-The difference between a School Street and other Low Traffic Neighbourhood intervention is that it is usually only active for certain hours of the
- day and week, to coincide with the times the school opens and closes.</blockquote>
+ <br>
+School Street schemes offer a proactive solution for school communities to tackle air pollution, poor health and road danger reduction. A School Street scheme will
+ encourage a healthier lifestyle and active travel to school for families and lead to a better environment for everyone.”
+ </blockquote>',
+   'section-2-text-col-2' => ' <div class="focus"><p>The difference between a School Street and other Low Traffic Neighbourhood intervention is that it is usually only active for certain hours of the day and week,
+ to coincide with the times the school opens and closes.<p></div>
+
+<p>Because the road is not fully closed, it remains a potential route for local drivers and for sat-nav routing outside these ‘closed’ school-run hours, and as
+such it could continue to be a ‘rat-run’ for pass-through traffic. For that reason it is important to have clear signage and to monitor adherence to keep all
+ road users safe. Ongoing measurement and enforcement will be necessary to see if additional measures need to be put in place.<p>
+',
 
 
-<p>Because the road is not fully closed, it remains a potential route for local drivers and for sat-nav routing outside these ‘closed’
-school-run hours, and as such it could continue to be a ‘rat-run’ for pass-through traffic. For that reason it is important to have clear
-signage and to monitor adherence to keep all road users safe. Ongoing measurement and enforcement will be necessary to see if additional
-measures need to be put in place.</p>
-
-</p> ',
-
-    'section-2-title' => 'What do we need to measure?',
-    'section-2-text' => '<p>Since the goals of a School Street
-<span  class="tiny">[<a href="[https://www.ciht.org.uk/blogs/school-streets-should-they-become-a-permanent-feature-of-our-landscape/">&#128279;</a>]</span>] are
-safer and more pleasant and healthy streets, it is important to measure both the changes in the number of vehicles that use the street, and their
-average speed. A separate parking study might also need to be done since much of the target traffic is not passing through, but arriving and departing.</p>
-
-<p>The measures of success will, of course, be a substantial drop in road traffic accidents involving children, and an improvement in air quality, but also
-an increase in the choice of active travel modes for travelling to and from the school for both children and their parents, so more bikes, scooters
-and walking.</p>
-
-<p>Since the School Street intervention is usually just for a single street, or street segment, it is also important to measure traffic effects and safety
-outcomes as well as air quality on the surrounding streets, since there is no benefit to improving one street simply by making the surrounding streets,
- that will also have to be used, more dangerous or polluted.<p>
-
-<p>In wider terms, school-run traffic represents a large percentage of car journeys on all urban roads in many cities, so as well as tracking the direct
-impact on the school and surrounding streets, it would be useful to measure the impact on all traffic on roads in the town or city, especially if there
-are several School Street projects underway in different zones. Removing school-run traffic could impact the road usage for other commuters and road-users
-during the same hours by reducing congestion, but also encouraging more active travel through the benefits of ‘safety in numbers’ (more cyclists means that
- individuals, particularly children, are more visible and hopefully less at risk of accidents).</p>
-
-<p>As in all mobility plans, to ensure that interventions and outcomes can be accurately attributed, and further enhancements (or corrections)
-can be carried out, it is important to have traffic counts before, during and after. Continuous measurement allows for more accurate solutions
-that are improved through feedback loops and local experience.</p>',
-
-    'section-3-title' => 'Putting plans into action',
-    'section-3-text' => '<p>The key audience for a School Street intervention will be the parents and teachers using the school. They must be engaged
- in the overall process to understand the benefits of the changes, and they are the ones who will be required to make behaviour changes.</p>
 
 
-<p>However, almost by definition, parents who currently drive to school will not be the ones living on the streets surrounding the school
- where measurements need to take place. Local residents are also exposed to the poor quality air and the congestion, and will hopefully benefit from changes,
- but they will also be affected by any restrictions. In order to get feedback and local support, it is critically important to involve the local
- community in the planning, tracking and development of solutions. </p>
+    'section-3-title' => 'What do you need to measure?',
+    'section-3-text' => '<p>Since the goals of a School Street [<a href="https://www.ciht.org.uk/blogs/school-streets-should-they-become-a-permanent-feature-of-our-landscape/"><span class="heroicon heroicon-external-link"></span></a>]
+are safer and more pleasant and healthy streets, it is important to measure both the changes in the number of vehicles that use the street, and their average speed. A separate
+ parking study might also need to be done since much of the target traffic is not passing through, but arriving and departing.
 
-<p>Involving local residents in data gathering, also known as Citizen Science, is key to Telraam’s process, not just because it gives access to be able
+<p>The measures of success will, of course, be a substantial drop in road traffic accidents involving children, and an improvement in air quality, but
+ also an increase in the choice of active travel modes for travelling to and from the school for both children and their parents, so more bikes,
+ scooters and walking.
+
+<h4>Surrounding streets</h4>
+<p>Since the School Street intervention is usually just for a single street, or street segment, it is also important to measure traffic effects
+ and safety outcomes as well as air quality on the surrounding streets, since there is no benefit to improving one street simply by making
+ the surrounding streets, that will also have to be used, more dangerous or polluted.
+
+<p>In wider terms, school-run traffic represents a large percentage of car journeys on all urban roads in many cities, so as well as tracking
+the direct impact on the school and surrounding streets, it would be useful to measure the impact on all traffic on roads in the town or
+city, especially if there are several School Street projects underway in different zones. Removing school-run traffic could impact the
+road usage for other commuters and road-users during the same hours by reducing congestion, but also encouraging more active travel
+through the benefits of ‘safety in numbers’ (more cyclists means that individuals, particularly children, are more visible and hopefully
+ less at risk of accidents).</p>
+
+<h4>Before, during and after</h4>
+<p>As in all mobility plans, to ensure that interventions and outcomes can be accurately attributed, and further enhancements (or corrections) can be carried out,
+it is important to have traffic counts before, during and after. Continuous measurement allows for more accurate solutions that are improved
+through feedback loops and local experience.
+ </p>',
+
+    'section-4-title' => 'Putting your plans into action',
+    'section-4-text' => '<p>The key audience for a School Street intervention will be the parents and teachers using the school. They must be engaged in the
+overall process to understand the benefits of the changes, and they are the ones who will be required to make behaviour changes. </p>
+
+<h4>Local residents vs citizens living further</h4>
+<p>However, almost by definition, parents who currently drive to school will not be the ones living on the streets surrounding the school where
+measurements need to take place. Local residents are also exposed to the poor quality air and the congestion, and will hopefully benefit from changes,
+ but they will also be affected by any restrictions. In order to get feedback and local support, it is critically important to involve the local community
+ in the planning, tracking and development of solutions.  </p>
+
+Involving local residents in data gathering, also known as Citizen Science, is key to Telraam’s process, not just because it gives access to be able
  to gather the data, but because it leads to more positive experiences and outcomes overall. In this case it creates the opportunity to bring together
-  the three groups (parents, residents & school) to work together to gather data and work on solutions together, a process called co-creation.</p>
+ the three groups (parents, residents & school) to work together to gather data and work on solutions together, a process called co-creation. </p>
 
-<p>If you are interested in school street interventions in your neighbourhood, town or city, then you should also make sure you also have the traffic data
-to support your plans and conclusions. </p>',
+<p>If you are interested in school street interventions in your neighbourhood, town or city, then you should also make sure you also have the traffic
+data to support your plans and conclusions.  </p>',
+
 
     'case-other' => 'Read the other cases:'
 
