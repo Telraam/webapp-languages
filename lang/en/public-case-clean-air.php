108217e87e6e44432abea1c9bcd34f89c7a1a9be
Dave Driesmans <dave@waanz.in>
2023-04-27 10:59:13 +0200

    Clean air

--- lang/en/public-case-clean-air.php
+++ lang/en/public-case-clean-air.php
@@ -2,66 +2,118 @@
 
 return [
 
-    'title' => 'Traffic counts for Clean Air',
+    // general
+    'examples-title' => 'Examples of Telraam applications',
+    'location' => 'Location',
+    'number-of-devices' => 'Devices',
+    'challenges' => 'Challenges',
+    'link' => 'Link',
+
+
+    'title' => 'Telraam traffic counts for Clean Air',
     'intro' => 'How does a Telraam traffic counting network help to achieve cleaner air in your street, neighbourhood or urban area?',
 
-    'section-1-title' => 'Clean air campaign goals',
-    'section-1-text' => '
-    <p>The overall goal of Clean Air campaigns <span class="tiny">[<a href="https://www.unep.org/explore-topics/air"  target="_blank">&#128279;</a>]</span> are to ensure that we all,
-    but especially children, get a chance to breathe air that is not unhealthy or unsafe from pollutants
-    <span class="tiny">[<a href="https://uk-air.defra.gov.uk/air-pollution/causes" target="_blank">&#128279;</a>]</span> including Nitrogen dioxide (NO2), Carbon monoxide (CO),
-    Sulphur dioxide (SO2) and Particular matters (PM10, PM2.5 and PM1).</p>
 
-<p>It has been estimated that such air pollution from fossil fuels is responsible for between 1 in every 9, and up to 1 in 5
- <span class="tiny">[<a href="https://www.hsph.harvard.edu/c-change/news/fossil-fuel-air-pollution-responsible-for-1-in-5-deaths-worldwide/"  target="_blank">
- &#128279;</a>]</span>,
- deaths in the world. The burning of fossil fuels in internal combustion engines (ICE) is a major contributor to this,
-  especially in the concentrated environment of cities in proximity to our living spaces. Cars are not the only culprit,
-  but they are the one we can most easily influence, and will have the most direct impact on individuals’ health.</p>
+    'example-1-title' => 'Compair',
+    'example-1-body' => 'COMPAIR, a European project, where Telraam traffic counting sensors are collocated with different types of air quality sensors. The goal of
+    the experiment is to understand if the affordable Telraam traffic counting sensor can supplement high-end air quality sensors.<br>
 
-<p>The three most important interventions to achieve these goals are:
-<ul class="jump-in">
-<li>Switch vehicles away from fossil fuels, for example to electric vehicles (EV)</li>
-<li>Reduce total number of vehicle miles driven, switching to active travel modes</li>
-<li>Increase mitigation measures, such as planting more trees or pollution-abating plants.</li>
-</ul></p> ',
+Telraam sensors are deployed alongside air quality sensors: SODAQ & OnePlanet NO2 AQ Sensor Prototype. These Telraam devices in Herzele will be used together with air quality monitoring systems to monitor the implementation of
+a school street. Additional deployment for pilot sites in Athens, Berlin and Sofia.',
 
-    'section-2-title' => 'What do we need to measure?',
-    'section-2-text' => '<p>Of course, it is important to measure air quality data to understand the issues we face, and where they are at
-their worst. However, while maps of air quality might highlight the link between poor air quality and roads and high traffic levels,
- on their own they will not distinguish between which interventions are having an impact.</p>
+    'example-1-location' => 'Herzele & Ghent (BE)',
+    'example-1-number' => '40 devices',
+    'example-1-challenges' => 'API, S2 prototype, Data',
+    'example-1-link' => 'https://www.wecompair.eu/',
 
-<p>If the measure is to reduce car journeys, then you also need to know how many cars are actually using the road, and when.
- Then you need to see which actions to reduce this are effective, such as legal speed restrictions, traffic calming, modal filters,
- behavioural messaging, road pricing, etc. Finally, you can match these interventions to the quality of the air that results.</p>
 
-<p>It is therefore important to track any interventions that are designed to change traffic volume not just the location of the intervention,
- but the surrounding streets as well. Like air movements, it is important to ensure traffic is not just reduced at a particular location through displacement.</p>
+    'example-2-title' => 'Canopea',
+    'example-2-body' => 'In a project for Canopea, a Belgian advocacy group, an ultra-fine particle sensor was installed close to an existing Telraam-device in Liège Métropole (BE).<br>
+ <br>Data-analysis confirmed the correlation between number of motorised vehicles (x-axis)
+    and ultra-fine particles detected (y-axis).',
 
-<p>Targeting volumes and speeds of vehicles is obviously of primary importance, but it is also essential to measure whether this is just a reduction in the
- number of car journeys on a road, or whether there is an active switch to alternative active travel modes, such as walking or cycling. </p>
+    'example-2-location' => 'Liège (BE)',
+    'example-2-number' => '20 devices',
+    'example-2-challenges' => 'API',
+    'example-2-link' => 'https://www.canopea.be/particules-ultrafines-et-science-citoyenne-dans-la-cite-ardente/',
 
-<p>In order to ensure that interventions and outcomes can be accurately attributed, and further enhancements (or corrections) can be carried out, it is
-important to have traffic counts before, during and after. Continuous measurement allows for more accurate solutions that are improved through feedback
+
+    'section-1-title' => 'Telraam x Clean air measures',
+    'section-1-text' => ' <p>Clean air is one of the main challenges faced by our urban areas in the 21st century. The level of premature death caused by air
+ pollution on a global scale means policy action on this matter is urgent
+  <a href="https://uk-air.defra.gov.uk/air-pollution/causes"><span class="heroicon heroicon-external-link"></span></a></p>
+
+<p>Car traffic, still dominated by combustion engines, remains one of the main contributors to air pollution and so is a key part of the
+solution to achieving clean air, and monitoring traffic is therefore an important element in any policy action on clean air initiatives.</p>
+
+<p>Deploying Telraam devices fulfils several essential roles in air quality monitoring activities:
+<ol>
+    <li>Telraam, as a device for monitoring local traffic volumes, complements direct air quality measurements.</li>
+    <li>Actions tackling air pollution often aim for a reduction of vehicle traffic, so Telraam delivers a detailed long-term tracking of air quality mitigation actions,
+    before and after implementation.
+    </li>
+    <li>By engaging citizens directly in monitoring activities using Telraam raises public awareness and creates ownership of solutions. </li>
+</ol>
+</p>',
+
+
+    'section-2-title' => 'Measuring air quality',
+    'section-2-text-col-1' => '<h4>Understand the issues</h4><p>It is important to measure air quality data to understand the issues we face, and where they are at their worst.
+However, while maps of air quality might highlight the link between poor air quality and roads and high traffic levels, on their own they do not
+distinguish between which interventions are having an impact. Also, air quality monitoring equipment is quite expensive, so air quality data is
+unavailable in most locations. </p>
+
+<p>If the objective is to reduce car journeys, then you need to know how many cars are actually using the road, and when. You also need to assess
+which actions to reduce this are effective, such as legal speed restrictions, traffic calming, modal filters, behavioural messaging, road pricing,
+ etc. Finally, you can match these interventions to the quality of the air that results. </p>',
+
+'section-2-text-col-2' => '<h4>Track interventions</h4><p>It is therefore important to track interventions designed to change traffic volume, not just the location of the intervention, but the
+surrounding streets as well. Like air movements, it is important to ensure traffic is not just reduced at a particular location through displacement.</p>
+
+<p>It is important to have traffic counts before, during and after, in order to ensure that interventions and outcomes can be accurately attributed, and
+further enhancements (or corrections) can be carried out. Continuous measurement allows for more accurate solutions that are improved through feedback
 loops and local experience.</p>',
+    
 
     'section-3-title' => 'Putting plans into action',
-    'section-3-text' => '<p>In order to get that feedback and local support, it is critically important to involve
-     the local community in the planning, tracking and development of solutions. The local community are those who are most
-     exposed to the poor quality air and toxic elements, and will be those who can benefit most directly from changes, and will
-     be motivated to take part in actions that they believe are positive and they can trust.</p>
+    'section-3-text' => '<p>In order to get feedback and local support, it is critically important to involve the local community in the planning,
+tracking and development of solutions. The local community are those who are most exposed to the poor quality air and toxic elements, and
+will be those who can benefit most directly from changes, and will be motivated to take part in actions that they believe are positive
+and they can trust.</p>
 
-<p>Involving local residents in data gathering, also known as Citizen Science, is key to Telraam’s process, not just because it
- gives access to be able to gather the data, but because it leads to more positive experiences and outcomes overall.</p>
+<h4>Citizen Science</h4>
+<p>Involving local residents in data gathering, also known as Citizen Science, is key to Telraam’s process, not just because it gives access to be
+able to gather the data, but because it leads to more positive experiences and outcomes overall.</p>
 
-<p>In practice, it is those who are most directly affected who also have the most direct views of the streets and traffic causing the
-air quality issues. They will be not only motivated to support the research, but will be less likely to resist any intervention because
- they will know what data has been gathered, when and how, and are more likely to feel ownership of the results.</p>
+<p>In practice, it is those who are most directly affected who also have the most direct views of the streets and traffic causing the air
+quality issues. They will be not only motivated to support the research, but will be less likely to resist any intervention because they
+will know what data has been gathered, when and how, and are more likely to feel ownership of the results.</p>
 
-<p>If you are interested in air quality interventions in your street, neighbourhood, town or city, then you should also make sure
-you also have the traffic data to support your plans and conclusions. </p>',
+<p>If you are interested in air quality interventions in your street, neighbourhood, town or city, then you should also make sure you
+also have the traffic data to support your plans and conclusions. </p>',
 
-    'case-other' => 'Read the other cases:'
+    'section-4-title' => 'Clean air campaigns',
+    'section-4-text' => '<p>The overall goal of Clean Air campaigns [<a href="https://www.unep.org/explore-topics/air"><span class="heroicon heroicon-external-link"></span></a>] are
+ to ensure that we all, but especially children, get a chance to breathe air that is not unhealthy or unsafe from
+  pollutants [<a href="https://uk-air.defra.gov.uk/air-pollution/causes"><span class="heroicon heroicon-external-link"></span></a>]
+ including Nitrogen dioxide (NO2), Carbon monoxide (CO), Sulphur dioxide (SO2) and Particular matters (PM10, PM2.5 and PM1).
+
+<h4>Impact</h4>
+<p>It has been estimated that such air pollution from fossil fuels is responsible for between 1 in every 9, and up to 1 in 5
+ [<a href="https://www.hsph.harvard.edu/c-change/news/fossil-fuel-air-pollution-responsible-for-1-in-5-deaths-worldwide/"><span class="heroicon heroicon-external-link"></span></a>], deaths in the world.
+  The burning of fossil fuels in internal combustion engines (ICE) is a major contributor to this, especially in the concentrated environment
+  of cities in proximity to our living spaces. Cars are not the only culprit, but they are the one we can most easily influence, and will
+  have the most direct impact on individuals’ health.</p>
+
+  <p>
+  The three most important interventions to achieve these goals are:
+  <li>Switch vehicles away from fossil fuels, for example to electric vehicles (EV)</li>
+  <li>Reduce total number of vehicle miles driven, switching to active travel modes</li>
+  <li>Increase mitigation measures, such as planting more trees or pollution-abating plants</li>
+</p>',
+
+
+    'case-other' => 'Read other case studies:'
 
 
 ];
