c641faede463a2c46b0525d0c9c58fdbf22e948a
telraamtalks <116150500+telraamtalks@users.noreply.github.com>
2023-04-27 12:47:24 +0100

    Update public-case-clean-air.php
    updated text

--- lang/en/public-case-clean-air.php
+++ lang/en/public-case-clean-air.php
@@ -14,12 +14,12 @@ return [
     'intro' => 'How does a Telraam traffic counting network help to achieve cleaner air in your street, neighbourhood or urban area?',
 
 
-    'example-1-title' => 'Compair',
-    'example-1-body' => 'COMPAIR, a European project, where Telraam traffic counting sensors are collocated with different types of air quality sensors. The goal of
-    the experiment is to understand if the affordable Telraam traffic counting sensor can supplement high-end air quality sensors.<br>
+    'example-1-title' => 'COMPAIR',
+    'example-1-body' => 'COMPAIR, a European project, where Telraam traffic counting sensors are co-located with different types of air quality sensors. The goal of
+    the experiment is to understand if the affordable Telraam traffic counting sensor can supplement high-end air quality sensors.<br><br>
 
 Telraam sensors are deployed alongside air quality sensors: SODAQ & OnePlanet NO2 AQ Sensor Prototype. These Telraam devices in Herzele will be used together with air quality monitoring systems to monitor the implementation of
-a school street. Additional deployment for pilot sites in Athens, Berlin and Sofia.',
+a school street. Additional deployment will happen in pilot sites in Athens, Berlin and Sofia.',
 
     'example-1-location' => 'Herzele & Ghent (BE)',
     'example-1-number' => '40 devices',
@@ -27,7 +27,7 @@ a school street. Additional deployment for pilot sites in Athens, Berlin and Sof
     'example-1-link' => 'https://www.wecompair.eu/',
 
 
-    'example-2-title' => 'Canopea',
+    'example-2-title' => 'CANOPEA',
     'example-2-body' => 'In a project for Canopea, a Belgian advocacy group, an ultra-fine particle sensor was installed close to an existing Telraam-device in Liège Métropole (BE).<br>
  <br>Data-analysis confirmed the correlation between number of motorised vehicles (x-axis)
     and ultra-fine particles detected (y-axis).',
@@ -38,13 +38,12 @@ a school street. Additional deployment for pilot sites in Athens, Berlin and Sof
     'example-2-link' => 'https://www.canopea.be/particules-ultrafines-et-science-citoyenne-dans-la-cite-ardente/',
 
 
-    'section-1-title' => 'Telraam x Clean air measures',
+    'section-1-title' => 'Telraam and Clean air measures',
     'section-1-text' => ' <p>Clean air is one of the main challenges faced by our urban areas in the 21st century. The level of premature death caused by air
  pollution on a global scale means policy action on this matter is urgent
   <a href="https://uk-air.defra.gov.uk/air-pollution/causes"><span class="heroicon heroicon-external-link"></span></a></p>
 
-<p>Car traffic, still dominated by combustion engines, remains one of the main contributors to air pollution and so is a key part of the
-solution to achieving clean air, and monitoring traffic is therefore an important element in any policy action on clean air initiatives.</p>
+<p>Car traffic, still dominated by combustion engines, remains one of the main contributors to air pollution and so dealing with it is a key part of the solution to achieving clean air. Monitoring traffic is therefore an important element in any policy action on clean air initiatives.</p>
 
 <p>Deploying Telraam devices fulfils several essential roles in air quality monitoring activities:
 <ol>
@@ -58,7 +57,7 @@ solution to achieving clean air, and monitoring traffic is therefore an importan
 
 
     'section-2-title' => 'Measuring air quality',
-    'section-2-text-col-1' => '<h4>Understand the issues</h4><p>It is important to measure air quality data to understand the issues we face, and where they are at their worst.
+    'section-2-text-col-1' => '<h4>Understanding the issues</h4><p>It is important to measure air quality data to understand the issues we face, and where they are at their worst.
 However, while maps of air quality might highlight the link between poor air quality and roads and high traffic levels, on their own they do not
 distinguish between which interventions are having an impact. Also, air quality monitoring equipment is quite expensive, so air quality data is
 unavailable in most locations. </p>
@@ -67,8 +66,8 @@ unavailable in most locations. </p>
 which actions to reduce this are effective, such as legal speed restrictions, traffic calming, modal filters, behavioural messaging, road pricing,
  etc. Finally, you can match these interventions to the quality of the air that results. </p>',
 
-'section-2-text-col-2' => '<h4>Track interventions</h4><p>It is therefore important to track interventions designed to change traffic volume, not just the location of the intervention, but the
-surrounding streets as well. Like air movements, it is important to ensure traffic is not just reduced at a particular location through displacement.</p>
+'section-2-text-col-2' => '<h4>Track interventions</h4><p>It is important to track interventions designed to change traffic volume, not just the location of the intervention, but the
+surrounding streets as well. Like air movement, it is important to ensure traffic is not just reduced at a particular location through displacement.</p>
 
 <p>It is important to have traffic counts before, during and after, in order to ensure that interventions and outcomes can be accurately attributed, and
 further enhancements (or corrections) can be carried out. Continuous measurement allows for more accurate solutions that are improved through feedback
@@ -99,11 +98,9 @@ also have the traffic data to support your plans and conclusions. </p>',
  including Nitrogen dioxide (NO2), Carbon monoxide (CO), Sulphur dioxide (SO2) and Particular matters (PM10, PM2.5 and PM1).
 
 <h4>Impact</h4>
-<p>It has been estimated that such air pollution from fossil fuels is responsible for between 1 in every 9, and up to 1 in 5
- [<a href="https://www.hsph.harvard.edu/c-change/news/fossil-fuel-air-pollution-responsible-for-1-in-5-deaths-worldwide/"><span class="heroicon heroicon-external-link"></span></a>], deaths in the world.
-  The burning of fossil fuels in internal combustion engines (ICE) is a major contributor to this, especially in the concentrated environment
-  of cities in proximity to our living spaces. Cars are not the only culprit, but they are the one we can most easily influence, and will
-  have the most direct impact on individuals’ health.</p>
+<p>The EEA reports in its latest annual TERM-report 
+ [<a https://www.eea.europa.eu//publications/europes-air-quality-status-2023"><span class="heroicon heroicon-external-link"></span></a>], NO2 concentrations above the EU annual limit value registered at 3% of all monitoring stations, but 75% of those reporting exceedances were traffic stations, with the highest concentrations found in some big cities with a high volume of traffic.
+ According to the EPA [<a https://www.epa.gov/no2-pollution/basic-information-about-no2"><span class="heroicon heroicon-external-link"></span></a>], this pollution, specifically from the type of emission from car traffic include respiratory diseases, impacts health and particularly asthma, leading to respiratory symptoms (such as coughing, wheezing or difficulty breathing). People with asthma, as well as children and the elderly, are generally at greater risk for the health effects of NO2.</p>
 
   <p>
   The three most important interventions to achieve these goals are:
