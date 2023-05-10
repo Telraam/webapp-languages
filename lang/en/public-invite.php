703ef203b15816dd9e4408aaa27f948ab6748548
Dave Driesmans <dave@waanz.in>
2023-05-10 16:21:39 +0200

    New content Invite Page

---
+++ lang/en/public-invite.php
@@ -0,0 +1,26 @@
+<?php
+
+return [
+    'intro' => 'Join the network to unlock more features.',
+
+    //
+    'body' => 'Telraam works best within a local network. Not joined a network here? You can do it here!',
+
+    // Not logged in
+    'not-logged-in-body' => 'Oh, but please login first or create an account so we can match account an accoung.',
+    'log-in' => 'Log in',
+    'or' => 'or',
+    'create-an-account' => 'Create an account',
+
+    // Logged in
+    'logged-in-body' => 'Joining a network is as simple as confirming here.',
+    'confirm' => 'Sure, approve!',
+
+    // already member
+    'you-are-already' => 'You are already a member of',
+    'are-you-sure' => 'Are you sure you want to switch to',
+
+    // thanks page
+    'thanks' => 'Great! You just joined the network!'
+
+];
