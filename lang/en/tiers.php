b87ec6f13b6c04b0a12cbb5b76769687ba4fccd5
Dave Driesmans <dave@waanz.in>
2023-05-10 10:18:35 +0200

    Tiers

---
+++ lang/en/tiers.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+        // user edit
+    'title'             => 'Tier',
+    'info'              => 'With a pro tier you get more data insight and you support the Telraam community. Organisations, cities or organisations are obligued to offer the citizens a pro account',
+    'pro-status'        => 'You have a device with pro tier.',
+    'basic-status'       => 'You are currently on a basic tier.',
+
+    'have-license'       => 'You have a license code to upgrade?',
+    'upgrade-to-pro'           => 'Upgrade to Pro (180€/year)',
+
+
+];
