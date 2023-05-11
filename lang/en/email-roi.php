872d77ebd89c0efe0a999e823ad478756950a212
Dave Driesmans <dave@waanz.in>
2023-05-11 15:20:39 +0200

    Roi email

---
+++ lang/en/email-roi.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    'subject'        => 'Your S2 device prefers a Region of Interest',
+
+    'addressing' => 'Hi'
+    
+    'body' => "You have succesfully installed a S2 Telraam device. Nice!<br>
+    
+    
+    However to make sure the device counts accuratly we would like you to select the region of interest (ROI) of your camera.<br><br>
+    
+    The S2 has a built-in, wide-angle camera and the image recognition works better if you can narrow down the view of the camera.  We don't want the device to count birds or trash cans but use all resources for the mobility in your street.<br>
+    <br>
+        
+    How to do it? We made an interface to do so easily:",
+        
+    'button' => 'Confirm your region of interest'
+   
+   'body-2' => 'If you have trouble setting the ROI or have additional questions feel to browse to 
+   <a href="https://telraam.helpspace-docs.io/article/66/region-of-interest-roi-and-roi-selection-with-telraam-s2"> our faq article</a> or email 
+   <a href="mailto:support@telraam.net">support@telraam.net</a>',
+   
+   'body-2' => 'If you have trouble setting the ROI or have additional questions feel to browse to 
+      <a href="https://telraam.helpspace-docs.io/article/66/region-of-interest-roi-and-roi-selection-with-telraam-s2"> our faq article</a> or email 
+      <a href="mailto:support@telraam.net">support@telraam.net</a>',
+
+
+];
