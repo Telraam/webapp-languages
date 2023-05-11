abcbccee974dba7c0513b31c2d282b25a3ee028b
Dave Driesmans <dave@waanz.in>
2023-05-11 14:06:07 +0200

    Invite

--- lang/en/public-invite.php
+++ lang/en/public-invite.php
@@ -7,18 +7,22 @@ return [
     'body' => 'Telraam works best within a local network. Not joined a network here? You can do it here!',
 
     // Not logged in
-    'not-logged-in-body' => 'Oh, but please login first or create an account so we can match account an accoung.',
+    'not-logged-in-body' => 'Oh, but please login first or create an account.<br> That way we can match your account with the network.',
     'log-in' => 'Log in',
     'or' => 'or',
     'create-an-account' => 'Create an account',
 
     // Logged in
     'logged-in-body' => 'Joining a network is as simple as confirming here.',
-    'confirm' => 'Sure, approve!',
+    'confirm' => 'Sure, I approve!',
 
-    // already member
+    // already member other network
     'you-are-already' => 'You are already a member of',
     'are-you-sure' => 'Are you sure you want to switch to',
+
+    // already member this network
+    'you-are-already-of-this' => 'Oh, but you are already a member of the network',
+    'you-can-not-join-twice' => 'You can not join twice',
 
     // thanks page
     'thanks' => 'Great! You just joined the network!'
