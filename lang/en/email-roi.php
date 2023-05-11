9988a2eaaefd97c20a759424aaf280f7bf014f90
Dave Driesmans <dave@waanz.in>
2023-05-11 15:56:13 +0200

    Roi email update via Slack :)

--- lang/en/email-roi.php
+++ lang/en/email-roi.php
@@ -1,29 +1,29 @@
 <?php
-
+​
 return [
-    'subject'        => 'Your S2 device prefers a Region of Interest',
-
+    'subject'        => 'Your S2 device works best if you set a Region of Interest',
+​
     'addressing' => 'Hi'
     
-    'body' => "You have succesfully installed a S2 Telraam device. Nice!<br>
+    'body' => "You have succesfully installed a Telraam S2 device. Thanks!<br>
     
     
-    However to make sure the device counts accuratly we would like you to select the region of interest (ROI) of your camera.<br><br>
+    However to make sure the device counts accurately we would like to ask you to select the best region of interest (ROI) for your camera.<br><br>
     
-    The S2 has a built-in, wide-angle camera and the image recognition works better if you can narrow down the view of the camera.  We don't want the device to count birds or trash cans but use all resources for the mobility in your street.<br>
+    The S2 has a built-in, wide-angle camera and the image recognition works best if you can narrow down the view of the camera.  We don't want the device to count birds or flying trash cans but use all its resources for tracking the activity in your street.<br>
     <br>
         
-    How to do it? We made an interface to do so easily:",
-        
+    How do you go about this? Well, we buit an interface to make it easy. Click below and select from the options:",
+      
     'button' => 'Confirm your region of interest'
    
-   'body-2' => 'If you have trouble setting the ROI or have additional questions feel to browse to 
+   'body-2' => 'If you have issue setting the ROI or have additional questions feel to browse to 
    <a href="https://telraam.helpspace-docs.io/article/66/region-of-interest-roi-and-roi-selection-with-telraam-s2"> our faq article</a> or email 
    <a href="mailto:support@telraam.net">support@telraam.net</a>',
    
    'body-2' => 'If you have trouble setting the ROI or have additional questions feel to browse to 
       <a href="https://telraam.helpspace-docs.io/article/66/region-of-interest-roi-and-roi-selection-with-telraam-s2"> our faq article</a> or email 
       <a href="mailto:support@telraam.net">support@telraam.net</a>',
-
-
+​
+​
 ];
