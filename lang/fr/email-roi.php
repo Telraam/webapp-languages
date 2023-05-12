443b1d18e6d920b877d70e3bf95d31c2a9c71261
Dave Driesmans <dave@waanz.in>
2023-05-12 12:31:18 +0200

    Roi character fix

--- lang/fr/email-roi.php
+++ lang/fr/email-roi.php
@@ -1,27 +1,15 @@
 <?php
-​
+
 return [
     'subject'        => 'Votre appareil S2 fonctionne mieux si vous définissez une région d\'intérêt',
-​
-    'addressing' => 'Bonjour'
-    
-    'body' => "Vous avez installé avec succès un appareil Telraam S2. Merci!<br>
-     
-     
-      Cependant, pour nous assurer que l'appareil compte avec précision, nous aimerions vous demander de sélectionner la meilleure région d'intérêt (ROI) pour votre appareil photo.<br><br>
-     
-      Le S2 dispose d'une caméra grand angle intégrée et la reconnaissance d'image fonctionne mieux si vous pouvez réduire la vue de la caméra. Nous ne voulons pas que l'appareil compte les oiseaux ou les poubelles volantes, mais utilise toutes ses ressources pour suivre l'activité dans votre rue.<br>
+     'addressing' => 'Bonjour',
+        'body' => "Vous avez installé avec succès un appareil Telraam S2. Merci!<br><br>
+          Cependant, pour nous assurer que l'appareil compte avec précision, nous aimerions vous demander de sélectionner la meilleure région d'intérêt (ROI) pour votre appareil photo.<br><br>
+                Le S2 dispose d'une caméra grand angle intégrée et la reconnaissance d'image fonctionne mieux si vous pouvez réduire la vue de la caméra. Nous ne voulons pas que l'appareil compte les oiseaux ou les poubelles volantes, mais utilise toutes ses ressources pour suivre l'activité dans votre rue.<br>
       <br>
-         
       Comment procédez-vous? Eh bien, nous avons construit une interface pour le rendre facile. Cliquez ci-dessous et sélectionnez parmi les options :",
-      
     'button' => 'Confirmez votre région d\'intérêt',
-   
    'body-2' => 'Si vous rencontrez des problèmes pour définir le retour sur investissement ou si vous avez des questions supplémentaires, n\'hésitez pas à parcourir
        <a href="https://faq-fr.helpspace-docs.io/article/144/region-dinteret-roi-et-selection-de-roi-avec-telraam-s2"> notre article FAQ</a> ou e-mail
        <a href="mailto:support@telraam.net">support@telraam.net</a>',
-      
-
-​
-​
 ];
