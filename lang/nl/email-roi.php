0f81c8cee5e123173aa9bc208bc1e815f191f4f5
Dave Driesmans <dave@waanz.in>
2023-05-12 10:29:34 +0200

    Roi nl & fr

---
+++ lang/nl/email-roi.php
@@ -0,0 +1,28 @@
+<?php
+​
+return [
+    'subject'        => 'Uw S2-apparaat werkt het best als u een interessegebied instelt',
+​
+    'addressing' => 'Beste'
+    
+    'body' => "U heeft met succes een Telraam S2 apparaat geïnstalleerd. Prima!<br>
+     
+     
+     Maar om ervoor te zorgen dat het apparaat nauwkeurig telt, willen we u vragen het beste interessegebied (ROI) voor uw camera te selecteren.
+     
+e S2 heeft een ingebouwde groothoekcamera en de beeldherkenning werkt het beste als je het zicht van de camera kunt verkleinen..  We willen niet dat het apparaat vogels of rondvliegende vuilnisbakken telt, maar al zijn middelen gebruiken om de activiteit in uw straat te volgen.
+         .<br>
+    <br>
+        
+  Hoe pak je dit aan? We hebben een web-interface gebouwd om het gemakkelijk in te stellen. Klik hieronder en maak een keuze uit de aangeboden opties:",
+      
+    'button' => 'Bevestig uw interessegebied'
+   
+   'body-2' => 'Als u problemen heeft met het instellen van de ROI of als u aanvullende vragen heeft, kunt u hiernaartoe bladeren
+    <a href="https://faq-nl.helpspace-docs.io/article/104/interessegebied-roi-en-roi-selectie-met-telraam-s2">ons faq-artikel</a> of e-mailen
+    <a href="mailto:support@telraam.net">support@telraam.net</a>',
+   
+
+​
+​
+];
