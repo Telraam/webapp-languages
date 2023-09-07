3de9cc9533e78535fd1d77f159814aca0070439c
Dave Driesmans <dave@waanz.in>
2023-09-07 14:41:32 +0200

    Website pro > data, new content

---
+++ lang/en/public-data.php
@@ -0,0 +1,110 @@
+<?php
+
+return [
+
+    'title' => 'Data',
+    'sub-title' => 'Drill down into your traffic counts with superior levels of data for a location.<br><br>
+        Unlock the full potential of a Telraam data subscription .
+        You\'ll gain access to segment and device level counts on a
+        finer temporal resolution, more modes of traffic, advanced APIs,
+        and a broad set of visualisations and analytic tools.',
+
+    'section-1-title' => 'More detail',
+    'section-1-text' => '
+   <p>Telraam data subscribers get access to count data that has four times higher temporal
+    resolution compared to our standard dataset, and an extended set of road user
+    classes when looking at modal shares. This higher resolution also means a much
+    reduced delay which enables PRO users to follow live traffic data much more real-time.</p>
+    <br>
+    <strong>15 minute resolution</strong><br>
+    15 minute data intervals for all data products, including count data, typical trend data, and for all analytic,
+    visualisation, and export tools and APIs.
+    <ul class="jump-in">
+        <li>Discover, track, and analyse traffic patterns and anomalies with greater clarity.</li>
+        <li>Ideal for studying short-lived events (school-street usage, congestions, accidents).</li>
+        <li>Live traffic with a much more real-time display (delay of only 15 minutes on device level, compared to a bit
+        over an hour for standard users with only segment level data).</li>
+    </ul>
+    <br>
+    <strong>10 different modes</strong><br>
+    Traffic counts per direction of travel for 10 different road user classes (standard users are
+    provided aggregated counts over the four main classes).<br>
+    The PRO classes are bicycles, buses, cars, light trucks, motorcycles, pedestrians, strollers, tractors,
+    trailers, and trucks.',
+
+    'section-2-title' => 'More tools, better insights, and complete control',
+
+    'section-2-intro-text' => 'Benefit from the experience of the team behind Telraam. Get access to a
+     range of advanced, purpose-made tools to explore and understand your data, facilitating and accelerating
+      your research.<br>
+      The PRO subscription includes access to the Data dashboard, the Advanced APIs, the Street sheets,
+       and other custom data export options.',
+
+
+    // the 4 data sections
+    'features-1-title' => 'Segments',
+    'features-1-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
+     Quisque in nisi convallis, pulvinar nisl et, commodo leo. Etiam vitae faucibus ipsum. Aliquam convallis erat sed nulla lobortis
+      pharetra et id lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+
+    'features-2-title' => 'Trends',
+    'features-2-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
+     Quisque in nisi convallis, pulvinar nisl et, commodo leo.  Ut tempor vestibulum massa, vel convallis nisl dictum vitae. Ut condimentum tortor sed condimentum pretium.
+     Donec laoreet tristique massa a dignissim. Etiam vitae faucibus ipsum.
+     ridiculus mus.',
+
+    'features-3-title' => '15 minute data',
+    'features-3-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
+     Quisque in nisi convallis, pulvinar nisl et, commodo leo.
+     Donec laoreet tristique massa a dignissim. Etiam vitae faucibus ipsum. Aliquam convallis erat sed nulla
+     lobortis pharetra et id lacus. ',
+
+    'features-4-title' => 'Installations',
+    'features-4-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
+     Quisque in nisi convallis, pulvinar nisl et, commodo leo.
+     Donec laoreet tristique massa a dignissim.
+     Sed nulla lobortis pharetra et id lacus. Orci varius natoque penatibus et magnis dis parturient montes,
+     nascetur ridiculus mus.',
+
+
+
+
+    // 3 extra features
+    'extra-features-1-title' => 'Street sheet',
+    'extra-features-1-text' => 'Our popular on-demand data product is also part of the data subscription. We provide
+    a compact, in-depth Excel-format summary for a device or segment, including insights, calculations,
+    and raw data for further review or analysis.<br>
+These reports give all users, experts and non-experts alike, a review of the data from a device or segment
+in order to help identify trends and anomalies. The sheet provides easy-to-digest information on
+modal splits, typical traffic patterns (for days and weeks), and also speed patterns.',
+
+    'extra-features-2-title' => 'Advanced APIs',
+    'extra-features-2-text' => 'Get access to exclusive, professional API end-points:
+    <ul class="jump-in">
+        <li>Integrate custom data connections into anything ranging from personalised dashboards to a full integration
+        with Business Intelligence (BI) data systems.</li>
+        <li>Custom data rate limits to enable more regular data refreshing, or access to a selected set of segments and
+        time periods.</li>
+        <li>Public endpoints with the 15 minute resolution and the 10 traffic modes (subject to Telraam S2 availability
+        for the given segment) where possible.</li>
+        <li>Additional endpoints exclusively for data subscribers, such as 5 km/h resolution speed histograms
+         and additional typical-traffic-derived data.</li>
+        <li>Potential for custom integrations and endpoints by agreement or collaboration with the Telraam Team.</li>
+    </ul>',
+
+    'extra-features-3-title' => 'RAW data export',
+    'extra-features-3-text' => 'Download RAW count data (in spreadsheets or as CSV files) to carry out more custom analyses.
+    <ul class="jump-in">
+        <li>The data export tool allows organisations that do not have large data lakes or BI systems to export,
+        repurpose and personalise reports from one or even multiple devices.</li>
+<li>The data exports include charts, calculations, typical values, and raw data from the device.
+<li>Adapt the reports to report items of particular relevance to your street:
+Modal shift patterns
+Speed changes before or after interventions
+Traffic volumes over time
+And more …
+</li>
+</ul>',
+
+
+];
