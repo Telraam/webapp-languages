450144f3ac6951ad35a2b653e779ffd9fb622b46
Dave Driesmans <dave@waanz.in>
2023-09-08 11:33:08 +0200

    Data free

---
+++ lang/en/public-data-free.php
@@ -0,0 +1,57 @@
+<?php
+
+return [
+
+    'title' => 'Limited data  <span class="black">- Free for personal use only</span>',
+    'sub-title' => '',
+
+    'section-1-title' => 'Open Data for all users and campaigners',
+    'section-1-text' => 'All Telraam devices come with the following data summaries as standard:<br>
+                <br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> Counts & direction of travel for 4 different grouped modes
+                <ul class="jump-in" style="padding-left: 40px;">
+                    <li>Pedestrians</li>
+                    <li>Two-wheelers (bikes and motorbikes)</li>
+                    <li>Cars</li>
+                    <li>Heavy vehicles (vans, trucks, buses and more)</li>
+                </ul>
+                <br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> Car speed data & charts<br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> 1 hour data interval reports<br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> 24 averages<br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> Monthly insight & summary report for device users via email<br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> Ability to compare your location data to other similar locations<br>
+                <span class=" heroicon heroicon-check-circle" style="position:relative; top:4px "></span> Standard API access<br>
+                <br>
+                The basic information from Telraam devices is standardised and open so that devices anywhere in the world
+                can be used for local engagement by their hosts, their communities and their local
+                authorities.<br>
+                <br>
+                Navigate to any device on the Telraam map, and you will be able to see data
+                for that street.<br>
+                <br>
+                This same information is available to the device owner, who will also receive a monthly report via email, and will have the ability to export their data in a spreadsheet so they can do some further analysis.
+                Additional data, insights, reports and exports are available via our data package.',
+
+
+    'section-2-title' => 'What makes free different from a data subscription?',
+    'section-2-text' => 'In both cases, the Telraam device hardware is the same.<br><br>
+                When you purchase the device with a data subscription, we are including a subscription to full package that takes full advantage of the capabilities of the Telraam S2 device, and covers the cost of data communications, additional dashboards and reports.
+                When you purchase a Personal device, you get the standard data and reports mentioned above but you are not paying for the data communications.',
+
+    'section-3-title' => 'Who is the free option intended for?',
+    'section-3-text' => ' While Telraam sets up networks of devices, both large and small, to gather
+                useful traffic data for neighbourhoods or even cities, there is an important
+                role for individual explorers and citizen scientists who can then raise
+                local issues.<br>
+                <br>
+                For this reason, Telraam uses revenue from our professional & data contracts
+                to cover the costs of managing your device - for life! As a private citizen,
+                you can still buy a single device and gather the data for as long as you like
+                to use for your own projects.<br><br>
+                Because of this subsidy and support, this is strictly for individual, private citizens. If you are
+                an organisation involved in campaigning on, consulting for, or managing local transport and mobility
+                 projects, then please check out our ‘Professionals’ packages.'
+
+
+];
