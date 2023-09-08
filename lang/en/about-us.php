b16bb7dcbdc7be249b282b324b60d0e8d39425ca
Dave Driesmans <dave@waanz.in>
2023-09-08 10:09:43 +0200

    About us

--- lang/en/about-us.php
+++ lang/en/about-us.php
@@ -7,13 +7,25 @@ return [
     'subtitle'          => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim tincidunt congue. Nulla lacinia ultricies lobortis.',
 
     'section-1-title'            => 'History',
-    'section1-block1-text'        => '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus dignissim tincidunt congue. Nulla lacinia ultricies lobortis.</p>
-<p>Quisque ornare sit amet mi nec vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Nunc ac pharetra risus. Quisque vulputate eu justo in malesuada. Nam gravida interdum eros, eget efficitur nisl dapibus vestibulum. Mauris feugiat sem eu venenatis pulvinar. Aenean ac luctus libero. Curabitur eu congue libero. Ut turpis lacus, varius vitae luctus nec, consectetur nec mi. Integer vehicula erat ultrices erat elementum, at lacinia nisl malesuada.
- Phasellus elementum magna ac nisl varius lobortis. Curabitur sed ultricies ligula. In vel dictum ante, quis pretium risus. Etiam porta sem metus, nec laoreet lectus viverra semper.</p>',
-    'section1-block2-text'        => '<p>Curabitur eu faucibus nisi. Ut pharetra felis quis facilisis luctus. Pellentesque a ipsum quis justo pharetra elementum. Nulla ipsum augue, venenatis quis sem eu,
-pulvinar ornare tellus. Vivamus feugiat arcu risus. Etiam consequat vestibulum augue, sit amet ultricies ex vehicula sed. Curabitur blandit nec sapien vitae aliquet.</p>
-<p>Nam gravida interdum eros, eget efficitur nisl dapibus vestibulum. Mauris feugiat sem eu venenatis pulvinar. Aenean ac luctus libero. Curabitur eu congue libero. Ut turpis lacus, varius vitae luctus nec, consectetur nec mi. Integer vehicula erat ultrices erat elementum, at lacinia nisl malesuada.
- Phasellus elementum magna ac nisl varius lobortis. Curabitur sed ultricies ligula. In vel dictum ante, quis pretium risus. Etiam porta sem metus, nec laoreet lectus viverra semper</p>',
+    'section1-block1-text'        => '<p>Telraam is the brainchild of two organisations based in Leuven (BE):
+<a href="https://www.tmleuven.be/en/" target="_blank">Transport & Mobility</a>
+Leuven (TML), a traffic engineering consultancy, and <a href="https://www.mobiel21.be/" target="_blank">Mobiel21</a>, an NGO focussing on citizen participation in the field of mobility.<p>
+
+<p>Seeing how citizens science projects have been succesful to collect valuable data in collaboration with citizens in other fields like
+air quality data, we thought: we should be able to do the same with traffic counting data! And so, in 2019 we embarked on our journey to
+first develop <a href="https://telraam.net/en/v1">a simple sensor</a> and later in
+2023 <a href="https://telraam.net/en/S2">a purpose built sensor</a> for collecting traffic counting data, together with citizens.</p>',
+
+    'section-2-title'            => 'Mission',
+    'section2-block1-text'        => '<p>Our mission at Telraam is to become an ever growing international platform that empowers and
+educates citizens to become the future technicians and traffic engineers of their communities, who can bring local knowledge to the table,
+and become a driving force in conversations and debates on urban mobility policies that affect their neighborhoods. </p>
+
+<p>We want to provide affordable solutions to connect citizens and their local governments, and facilitate the shift towards a more
+human-focused, multimodal traffic culture. </p>',
+
+'section2-block2-text'        => '<p>Coming from a traffic engineering background, we know traffic counting data. We breathe traffic counting data. We understand what type of data can be valuable for professionals. Are you a mobility professional; a non-profit organization, a traffic engineering or consultancy firm, a transport agency, or a local government? Are you looking for affordable, multimodal traffic
+monitoring solutions for your projects that can be deployed on a large number of locations? Then Telraam is the right place for you!</p>',
 
     // team
     'team-title'   => 'Team',
