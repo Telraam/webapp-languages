a10415b9a8f70326451dbfaa920202c4c5f1ff94
Péter I. Pápics <papics@gmail.com>
2023-09-08 18:14:46 +0200

    Update public-data.php
    Added description of the 4 tabs, some text modifications, and an additional comment about formatting around the 4 tabs.

--- lang/en/public-data.php
+++ lang/en/public-data.php
@@ -3,15 +3,13 @@
 return [
 
     'title' => 'Data',
-    'sub-title' => 'Drill down into your traffic counts with superior levels of data for a location.<br><br>
-        Unlock the full potential of a Telraam data subscription .
-        You\'ll gain access to segment and device level counts on a
-        finer temporal resolution, more modes of traffic, advanced APIs,
-        and a broad set of visualisations and analytic tools.',
+    'sub-title' => 'Professional applications require advanced data products. Dig deeper with our Telraam data subscription, 
+    and unlock the full potential of the Telraam S2 device: gain access to segment and device level counts on a finer temporal resolution, 
+    more modes of traffic, advanced APIs, and a broad set of visualisations and analytic tools.',
 
     // pricing box
     'pricing-1-title' => 'Data subscription <span class="green">€15 <span class="month">/ month</span>',
-    'pricing-1-subtitle' => 'Get the 10 modes, greater detail and better reports',
+    'pricing-1-subtitle' => 'Get the 10 modes, greater detail, and better reports',
     'pricing-1-info' => '(Multiple) devices in your neighborhood as an organisation or city?
                             <div style="padding-left: 20px">
                                 We\'ll refer you to the <a href="../professionals">Network</a>.
@@ -27,57 +25,75 @@ return [
    <p>Telraam data subscribers get access to count data that has four times higher temporal
     resolution compared to our standard dataset, and an extended set of road user
     classes when looking at modal shares. This higher resolution also means a much
-    reduced delay which enables PRO users to follow live traffic data much more real-time.</p>
+    reduced delay which enables professional users to follow live traffic data much more real-time.</p>
     <br>
     <strong>15 minute resolution</strong><br>
-    15 minute data intervals for all data products, including count data, typical trend data, and for all analytic,
+    15 minute temporal resolution for all data products, including count data, typical trend data, and for all analytic,
     visualisation, and export tools and APIs.
     <ul class="jump-in">
         <li>Discover, track, and analyse traffic patterns and anomalies with greater clarity.</li>
         <li>Ideal for studying short-lived events (school-street usage, congestions, accidents).</li>
         <li>Live traffic with a much more real-time display (delay of only 15 minutes on device level, compared to a bit
-        over an hour for standard users with only segment level data).</li>
+        over an hour for non-subscriber users with only segment level data).</li>
     </ul>
     <br>
     <strong>10 different modes</strong><br>
-    Traffic counts per direction of travel for 10 different road user classes (standard users are
+    Traffic counts per direction of travel for 10 different road user classes (non-subscriber users are
     provided aggregated counts over the four main classes).<br>
-    The PRO classes are bicycles, buses, cars, light trucks, motorcycles, pedestrians, strollers, tractors,
-    trailers, and trucks.',
+    The differentiated classes are bicycles, buses, cars, light trucks, motorcycles, pedestrians, strollers, tractors,
+    trailers, and trucks.
+    <br>
+    <strong>Typical traffic data</strong><br>
+    Typical traffic is the foundation of many of the analytical tools and data products that we offer to our subscribers.
+    As an example, typicala data includes the typical (average) counts (and the most likely range) for the expected car traffic for a Monday morning 
+    between 08:00 and 08:15 based on the last 12 months\' actual traffic - but you can choose a different mode, day of week, time of day, and baseline 
+    period as you wish. How does this work in practice?
+    <ul class="jump-in">
+        <li>We provide typical traffic volumes calculated as the average (and standard deviation) of historical traffic counts over 
+        the periods of a year, a season, and a month leading up to the date of the calculation (referred to as the epoch).</li>
+        <li>Each typical product is calculated at regular intervals to provide a continuously growing set of historical 
+        averages corresponding to various epochs. The data is made available on a quarterly and hourly resolution, 
+        for each mode, down to each day of the week (including a more general weekday and weekend option too).</li>
+    </ul>
+    
+',
 
     'section-2-title' => 'More tools, better insights, and complete control',
 
     'section-2-intro-text' => 'Benefit from the experience of the team behind Telraam. Get access to a
      range of advanced, purpose-made tools to explore and understand your data, facilitating and accelerating
       your research.<br>
-      The PRO subscription includes access to the Data dashboard, the Advanced APIs, the Street sheets,
+      The data subscription includes access to the Data dashboard, the Advanced APIs, the Street sheets,
        and other custom data export options.',
 
+    // The 4 feature sections also need a title here: "Data dashboard" (optimally this would have the same weight as the Street sheets, APIs, and RAW data export below it,
+    // since these are the referred to topics from section-2-intro-text, then change the colour of the Segments/Trends/15 minute data/Installations) to green, and maybe smallcaps style.
 
     // the 4 data sections
     'features-1-title' => 'Segments',
-    'features-1-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
-     Quisque in nisi convallis, pulvinar nisl et, commodo leo. Etiam vitae faucibus ipsum. Aliquam convallis erat sed nulla lobortis
-      pharetra et id lacus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.',
+    'features-1-text' => 'Monitor live or historical traffic (and the overall modal split) on a segment and compare it to the expected levels to identify atypical traffic. For example: if the live traffic is outside 
+    of the interval that is defined by the average plus and minus three times the standard deviation of the histroical traffic, that means that the observed traffic 
+    is unexpected compared to the average of the past month, season, or year, depending on the used typical data product. Is the anomaly short lived or long-term,
+    is it caused by a special event, weather, or a change in the circulation plan? Go and explore!',
 
     'features-2-title' => 'Trends',
-    'features-2-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
-     Quisque in nisi convallis, pulvinar nisl et, commodo leo.  Ut tempor vestibulum massa, vel convallis nisl dictum vitae. Ut condimentum tortor sed condimentum pretium.
-     Donec laoreet tristique massa a dignissim. Etiam vitae faucibus ipsum.
-     ridiculus mus.',
+    'features-2-text' => 'Monitor the evolution of traffic trends over time. For example you can visualise 
+    the average Monday\'s car traffic over the past year, using seasonal (quarterly) averages that were calculated every two weeks, 
+    providing a rolling average traffic pattern over time. Looking at rolling averages helps to filter out short lived anomalies while bringing 
+    long-term changes into the spotlight. While the 3D histogram is a very powerful visualisation tool, if you just want to compare the traffic of a 
+    few selected epochs, we provide a traditional interactive 2D plot for that too. With the number of modes, weekdays, typical intervals, and calculation 
+    epochs available, the options are very broad.',
 
     'features-3-title' => '15 minute data',
-    'features-3-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
-     Quisque in nisi convallis, pulvinar nisl et, commodo leo.
-     Donec laoreet tristique massa a dignissim. Etiam vitae faucibus ipsum. Aliquam convallis erat sed nulla
-     lobortis pharetra et id lacus. ',
+    'features-3-text' => 'Similar to the Segments tab, but here you can monitor data with a higher temporal resolution. Some effects are very short-lived, 
+    for example the morning rush near schools, or a temporary road block caused by an accident. Quarterly live data is also much more real time, as new datapoints 
+    are added evey fifteen minutes, which is great for real-time applications.',
 
     'features-4-title' => 'Installations',
-    'features-4-text' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis lacus tellus.
-     Quisque in nisi convallis, pulvinar nisl et, commodo leo.
-     Donec laoreet tristique massa a dignissim.
-     Sed nulla lobortis pharetra et id lacus. Orci varius natoque penatibus et magnis dis parturient montes,
-     nascetur ridiculus mus.',
+    'features-4-text' => 'It happens that one road segment has more than one Telraam sensor installed. In such case the segment page shows the (smartly weighted) 
+    average metrics coming from the corresponding installations (which we refer to as Telraam instances). As a data subscriber you can consult the original data 
+    of each individual Telraam instance, which can be useful in many situations, including data validation, separating data coming from different sensor versions 
+    (e.g., Telraam V1 and S2 devices on the same segment), or simply to consult the data of your own device.',
 
 
 
@@ -98,7 +114,7 @@ modal splits, typical traffic patterns (for days and weeks), and also speed patt
         with Business Intelligence (BI) data systems.</li>
         <li>Custom data rate limits to enable more regular data refreshing, or access to a selected set of segments and
         time periods.</li>
-        <li>Public endpoints with the 15 minute resolution and the 10 traffic modes (subject to Telraam S2 availability
+        <li>Enhanced public endpoints with the 15 minute resolution and the 10 traffic modes (subject to Telraam S2 availability
         for the given segment) where possible.</li>
         <li>Additional endpoints exclusively for data subscribers, such as 5 km/h resolution speed histograms
          and additional typical-traffic-derived data.</li>
