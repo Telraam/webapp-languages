e1c8a837e5b5f51128e4fe6b2061e942f9e939e8
Dave Driesmans <dave@waanz.in>
2023-09-14 09:28:01 +0200

    EN user data dashboard (kickoff)

---
+++ lang/en/user-admin-data-dashboard.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'title'        => 'Data dashboard',
+    'info' => 'Is there much or little traffic in your street and how does your street compare to other streets in terms of morning and evening peak traffic? ',
+
+    'tab-1'        => 'Segments',
+    'tab-2'        => 'Trends',
+    'tab-3'        => '15 minute data',
+    'tab-4'        => 'Installations',
+
+
+];
