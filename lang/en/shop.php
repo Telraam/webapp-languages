c74c37730d804deb99a7f2608afaf1bf4677123d
Péter I. Pápics <papics@gmail.com>
2023-09-15 18:15:29 +0200

    Update shop.php
    Textual clean up, some minor changes/suggestions for Dave to review.

--- lang/en/shop.php
+++ lang/en/shop.php
@@ -9,68 +9,70 @@ return [
     'data-title'          => 'Data',
     'network-title'       => 'Network',
     'data-features'       => '<p><strong>Includes:</strong></p><ul class="pricing-checklist">
-                            <li>10 modes</li>
-                            <li>15 minute resolution</li>
+                            <li>10 road user classes</li>
+                            <li>15 minute data resolution</li>
                             <li>Data dashbpoard</li>
                             <li>Advanced APIs</li>
-                            <li>Street Sheet</li>
+                            <li>Street sheet</li>
+                            <li>Data export options</li>
                         </ul>',
     'network-features'       => '<p><strong>Includes:</strong></p><ul class="pricing-checklist">
-                            <li>Everything from data</li>
+                            <li>Everything from Data</li>
                             <li>Premium analytics tools</li>
                             <li>Premium reports</li>
                             <li>Manage multiple tracks</li>
-                            <li>Everything to support a network of devices</li>
+                            <li>Network recruiting tools</li>
+                            <li>Network support tools</li>
                         </ul>',
 
     // pre-made packages
-    'packages-title' => 'Pre-made packages',
-    'intro-packages'          => 'Get your network up and running today. To get you counting quickly, we currently offer some ready-made packages in the shop.',
+    'packages-title' => 'Pre-configured packages',
+    'intro-packages'          => 'Get started with Telraam today. To get you counting quickly, we offer some ready-made packages in the shop.',
 
     'warning-professional'  => 'Second production batch is completely sold out.<br> Estimated ship date for current orders: mid july 2023.',
 
     // warning
-    'warning-body-1' => '<strong>The Telraam device needs a clear view on the street.</strong>
+    'warning-body-1' => '<strong>The Telraam S2 device needs a clear view of the street.</strong>
 Review the <a href="https://telraam.helpspace-docs.io/article/3/requirements-for-the-installation-of-a-telraam-in-my-window" target="_blank" class="grey">requirements for the window placement</a>
     of your device(s).',
-    'warning-body-2' => '<strong>There needs to be a suitable LTE-M data network coverage</strong>. Check operator coverage maps for your area/country if you are unsure, or
+    'warning-body-2' => '<strong>We rely on cellular data-transfer via LTE-M or NB-IoT networks.</strong>. Check operator coverage maps for your area/country if you are unsure, or
 reach out to us <a href=mailto:support@telraam.net?body=coverage class="grey">via email</a> so we can assist.',
 
 
  //   'product-1-title' => 'Telraam S2 - Professional',
     'product-1-title' => 'The Pionieer',
-    'product-1-description' => 'One S2 sensor with 12 months data subscription',
+    'product-1-description' => 'One Telraam S2 with a 12 month Data subscription',
     'product-1-price' => '€340',
-    'product-1-note' => 'excl vat',
-    'product-1-link' => 'Pay',
+    'product-1-note' => 'excl. VAT',
+    'product-1-link' => 'Buy',
 
     'product-2-title' => 'The First Four',
-    'product-2-description' => 'Four S2 sensors with 12 months data subscription',
+    'product-2-description' => 'Four Telraam S2 devices with 12 month Data subscriptions',
     'product-2-price' => '€1,360',
-    'product-2-note' => 'excl vat',
-    'product-2-link' => 'Pay',
+    'product-2-note' => 'excl. VAT',
+    'product-2-link' => 'Buy',
 
-    'product-3-title' => 'Small Network',
-    'product-3-description' => 'Ten S2 devices with 12 months network plan.',
+    'product-3-title' => 'The Small Network',
+    'product-3-description' => 'Ten Telraam S2 devices with 12 month Network plans',
     'product-3-price' => '€3,990',
-    'product-3-note' => 'excl vat',
-    'product-3-link' => 'Pay',
+    'product-3-note' => 'excl. VAT',
+    'product-3-link' => 'Buy',
 
     // other / custom
-    'custom-packages-title'    => 'Custom package or question?',
-    'custom-packages-intro'  => '<p>Do not hesitate to <a href="https://telraam.net/en/contact-us">get in touch with us</a> for quotes, information or questions.
- We love to hear from you.</p>',
+    'custom-packages-title'    => 'Custom-made package or questions?',
+    'custom-packages-intro'  => '<p>You need more devices with a Data plan, or would you like to build a bigger network? Please <a href="https://telraam.net/en/contact-us">get in touch with us</a> for a personalised quote or more detailed information.
+ We would love to hear from you.</p>',
 
     // personal
     'personal-title' => 'Telraam S2 with basic data',
-    'personal-intro-personal' => 'As an individual you can buy a single device at a discounted rate - for personal use only -
-    to track your own street. This does not include the full data subscription options.',
+    'personal-intro-personal' => 'As an individual you can purchase a single Telraam S2 device at a discounted rate - for personal use only -
+    to count traffic on your street. This option does not include the perks of the Data subscription.',
 
     'product-4-title' => 'Telraam S2 - Personal',
-    'product-4-description' => 'Small, smart, and powerful. Effortless traffic counting on every street.',
+    'product-4-description' => 'Small, smart, and powerful. Effortless traffic counting on your street.',
     'product-4-price' => '€199',
-    'product-4-note' => 'Incl vat',
-    'product-4-link' => 'Pay',
+    'product-4-note' => 'incl. VAT',
+    'product-4-link' => 'Buy',
 
 
 ];
