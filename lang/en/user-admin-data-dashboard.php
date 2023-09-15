b9f1c8926e389b2ec0e49f4afafefa5bbca96a29
Dave Driesmans <dave@waanz.in>
2023-09-15 16:48:54 +0200

    User exports (EN)

--- lang/en/user-admin-data-dashboard.php
+++ lang/en/user-admin-data-dashboard.php
@@ -1,13 +1,18 @@
 <?php
 
 return [
+
+    // data dashboard
     'title'        => 'Data dashboard',
-    'info' => 'Is there much or little traffic in your street and how does your street compare to other streets in terms of morning and evening peak traffic? ',
+    'info'          => 'Is there much or little traffic in your street and how does your street compare to other streets in terms of morning and evening peak traffic? ',
+
+    'btn-open'      => 'Open',
+    'btn-not-ok'    => 'Only available for users with a data subscription',
+    'not-yet-available' => 'Not yet available',
 
     'tab-1'        => 'Segments',
     'tab-2'        => 'Trends',
     'tab-3'        => '15 minute data',
     'tab-4'        => 'Installations',
-
 
 ];
--- lang/en/user-export.php
+++ lang/en/user-export.php
@@ -10,8 +10,18 @@ return [
     'date-requested'   => 'Date Requested',
     'subject'          => 'Subject',
     'filename'         => 'Filename',
-    'empty'           => 'Nothing generated yet...',
-    'download'           => 'Download',
-    'processed'           => 'Processed',
+    'empty'            => 'Nothing generated yet...',
+    'download'         => 'Download',
+    'processed'        => 'Processed',
+
+    'only-for-data' => 'Only for users with data subscription',
+    'setup-is-not-completed' => 'Setup is not completed yet',
+
+    // street sheet
+    'street-sheet'  => 'Street Sheet',
+    'street-sheet-info' => 'Our Street sheets are prefilled Excel files that allow you to play with some counting data without the need of accessing our API.
+It makes use of raw counting data and typical data.<br>
+<br>
+Street sheets are now in beta. Feel free to share your feedback with us on <a href="mailto:support@telraam.net">support@telraam.net</a>'
 
 ];
