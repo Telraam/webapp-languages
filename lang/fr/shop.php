d18304018b17bbb01a6257cd9800873b6074662f
telraamtalks <116150500+telraamtalks@users.noreply.github.com>
2023-10-03 16:27:12 +0100

    corrections and update
    translation update

--- lang/fr/shop.php
+++ lang/fr/shop.php
@@ -12,13 +12,13 @@ return [
     'data-features' => '<p><strong>Inclus :</strong></p><ul class="pricing-checklist">
                             <li>10 catégories d\'usagers de la route</li>
                             <li>Résolution de données de 15 minutes</li>
-                            <li>Panneau de données</li>
-                            <li>APIs avancées</li>
-                            <li>Feuille de rue</li>
+                            <li>Tableau de bord de données</li>
+                            <li>API avancées</li>
+                            <li>Feuille de rue (Street Sheet)</li>
                             <li>Options d\'exportation de données</li>
                         </ul>',
     'network-features' => '<p><strong>Inclus :</strong></p><ul class="pricing-checklist">
-                            <li>Tout ce qui est inclus dans Data</li>
+                            <li>Tout ce qui est inclus dans Données</li>
                             <li>Outils d\'analyse premium</li>
                             <li>Rapports premium</li>
                             <li>Gestion de plusieurs pistes</li>
@@ -32,31 +32,31 @@ return [
     'warning-professional' => 'La deuxième production est entièrement épuisée. Date d\'expédition estimée pour les commandes actuelles : mi-juillet 2023.',
 
     'warning-body-1' => '<strong>L\'appareil Telraam doit avoir une vue dégagée sur la rue.</strong><br> Consultez les <a href="https://faq-fr.helpspace-docs.io/article/147/conditions-a-respecter-pour-lemplacement-de-ma-fenetre-photo-a-uploader-lors-de-lenregistrement" class="grey" target="_blank">conditions à respecter pour l\'emplacement</a> de votre (vos) appareil(s).',
-    'warning-body-2' => '<strong>La couverture du réseau de données LTE-M doit être adéquate</strong>. <br> Vérifiez les cartes de couverture de l\'opérateur pour votre région/pays si vous n\'êtes pas sûr, ou contactez-nous <a href=mailto:support@telraam.net?body=coverage class="grey">par e-mail</a> pour que nous puissions vous aider.',
+    'warning-body-2' => '<strong>La couverture du réseau mobile LTE-M doit être adéquate</strong>. <br> Vérifiez les cartes de couverture de l\'opérateur pour votre région/pays si vous n\'êtes pas sûr, ou contactez-nous <a href=mailto:support@telraam.net?body=coverage class="grey">par e-mail</a> pour que nous puissions vous aider.',
 
     'product-1-title' => 'Le Pionnier',
-    'product-1-description' => 'Un appareil Telraam S2 avec un abonnement Data de 12 mois',
+    'product-1-description' => 'Un appareil Telraam S2 avec un abonnement Données de 12 mois',
     'product-1-price' => '€340',
     'product-1-note' => 'hors TVA',
     'product-1-link' => 'Acheter',
 
     'product-2-title' => 'Les Quatre Premiers',
-    'product-2-description' => 'Quatre appareils Telraam S2 avec des abonnements Data de 12 mois',
+    'product-2-description' => 'Quatre appareils Telraam S2 avec des abonnements Données de 12 mois',
     'product-2-price' => '€1,360',
     'product-2-note' => 'hors TVA',
     'product-2-link' => 'Acheter',
 
     'product-3-title' => 'Le Petit Réseau',
-    'product-3-description' => 'Dix appareils Telraam S2 avec des forfaits Network de 12 mois',
+    'product-3-description' => 'Dix appareils Telraam S2 avec des abonnements Réseau de 12 mois',
     'product-3-price' => '€3,990',
     'product-3-note' => 'hors TVA',
     'product-3-link' => 'Acheter',
 
     'custom-packages-title' => 'Forfait sur mesure ou des questions ?',
-    'custom-packages-intro' => '<p>Vous avez besoin de plus d\'appareils avec un abonnement Data, ou souhaitez-vous construire un réseau plus important ? Veuillez <a href="https://telraam.net/en/contact-us">nous contacter</a> pour un devis personnalisé ou des informations plus détaillées. Nous serions ravis d\'avoir de vos nouvelles.</p>',
+    'custom-packages-intro' => '<p>Avez-vous besoin de plus d\'appareils avec un abonnement Données, ou souhaitez-vous construire un réseau plus important ? Veuillez <a href="https://telraam.net/en/contact-us">nous contacter</a> pour un devis personnalisé ou des informations plus détaillées. Nous serions ravis d\'avoir de vos nouvelles.</p>',
 
     'personal-title' => 'Telraam S2 avec données de base',
-    'personal-intro-personal' => 'En tant que particulier, vous pouvez acheter un seul appareil Telraam S2 à un tarif réduit - à usage personnel uniquement - pour compter le trafic dans votre rue. Cette option n\'inclut pas les avantages de l\'abonnement Data.',
+    'personal-intro-personal' => 'En tant que particulier, vous pouvez acheter un seul appareil Telraam S2 à un tarif réduit - à usage personnel uniquement - pour compter le trafic dans votre rue. Cette option n\'inclut pas les avantages de l\'abonnement Données.',
 
     'product-4-title' => 'Telraam S2 - Personnel',
     'product-4-description' => 'Petit, intelligent et puissant. Comptage du trafic sur votre rue sans effort.',
