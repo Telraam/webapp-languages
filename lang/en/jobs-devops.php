030019fdec203cba461ef929cbd9f229e9c521a9
Dave Driesmans <dave@waanz.in>
2023-11-20 15:17:32 +0200

    Jobs

---
+++ lang/en/jobs-devops.php
@@ -0,0 +1,42 @@
+<?php
+
+return [
+
+    'title'             => 'Cloud DevOps Engineer',
+
+    'responsibilities-title' => 'Your mission & key responsibilities, should you choose to accept it:',
+    'responsibilities-text' => '<ul>
+<li>Maintenance of the backend of Telraam in AWS (multiple production and testing
+databases, API Gateways, backups, scheduled and conditional services, security,
+load management, etc.)</li>
+<li>Manage, prepare, and schedule software updates of the backend (Postgres,
+python versions, layers, and libraries, etc...)</li>
+<li>Development and deployment of proprietary edge-device firmware updates (C++)</li>
+<li>Proactively search, develop, and implement incremental improvements to the
+AWS backend as well as the device firmware</li>
+</ul>',
+
+    'profile-title' => 'Profile:',
+    'profile-text' => '<ul>
+<li>At least bachelor ICT</li>
+<li>A minimum of 2 years experience with operating/maintaining cloud infrastructure
+(Azure, AWS,...) is a requirement.</li>
+<li>As Telraam infrastructure is in AWS, prior experience with AWS is a big plus.</li>
+<li>Fluent python programmer. Knowledge of C++ is a big plus.</li>
+<li>You are comfortable operating in a fast-paced startup environment</li>
+<li>Report to data scientist</li>
+</ul>',
+
+    'foryou-title' => 'What’s in it for you?',
+    'foryou-text' => '<ul>
+<li>Full-time (flexible)</li>
+<li>You will be part of an exciting, visible and impactful start-up</li>
+<li>The opportunity to grow and help shape the direction of the start-up</li>
+<li>A positive and open company culture</li>
+<li>A competitive salary and benefits package</li>
+<li>Flexible working hours and ample opportunity to work from home.</li>
+</ul>',
+
+    'closing' => 'Interested? <a mailto:jobs@telraam.net>jobs@telraam.net</a>'
+
+];
---
+++ lang/nl/jobs-devops.php
@@ -0,0 +1,43 @@
+<?php
+
+return [
+
+    'title'             => 'Cloud DevOps Engineer',
+
+    'responsibilities-title' => 'Verantwoordelijkheden:',
+    'responsibilities-text' => '<ul>
+<li>Onderhoud van de backend van Telraam in AWS (meerdere productie- en test
+databases, API Gateways, back-ups, geplande en conditionele services,
+beveiliging, load management, enz.)</li>
+<li>Beheren, voorbereiden en plannen van software-updates van de backend</li>
+(Postgres, pythonversies, lagen en bibliotheken, enzovoort)</li>
+<li>Ontwikkeling en implementatie van firmware-updates voor sensoren (C++)</li>
+<li>Proactief zoeken, ontwikkelen en implementeren van incrementele verbeteringen
+aan de AWS backend en de device-firmware</li>
+</ul>',
+
+    'profile-title' => 'Profiel',
+    'profile-text' => '<ul>
+<li>Minimaal bachelor ICT
+<li>Minstens 2 jaar ervaring met het beheren/onderhouden van cloud infrastructuur
+(Azure, AWS,...) is een vereiste.</li>
+<li>Aangezien de infrastructuur van Telraam in AWS is opgebouwd, is eerdere
+ervaring met AWS een groot pluspunt.</li>
+<li>Vloeiend python programmeur. Kennis van C++ is een groot pluspunt.</li>
+<li>Je voelt je op je gemak in een snelle startup-omgeving</li>
+<li>Rapporteren aan data scientist</li>
+</ul>',
+
+    'foryou-title' => 'Aanbod',
+    'foryou-text' => '<ul>
+<li>Fulltime (flexibel)</li>
+<li>Je maakt deel uit van een spannende, zichtbare en impactvolle start-up</li>
+<li>De kans om te groeien en de richting van de start-up mee vorm te geven</li>
+<li>Een positieve en open bedrijfscultuur</li>
+<li>Een competitief salaris en voordelenpakket</li>
+<li>Flexibele werktijden en voldoende mogelijkheden om thuis te werken.</li>
+</ul>',
+
+    'closing' => 'Interested? <a mailto:jobs@telraam.net>jobs@telraam.net</a>'
+
+];
