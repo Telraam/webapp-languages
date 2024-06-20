a67df79d257e8b1168fbd5547c78e528ae89bebe
Dave Driesmans <dave@waanz.in>
2024-06-20 15:32:50 +0200

    Set up changes — to do in FR / NL

---
+++ lang/en/email-outboard.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'addressing' => 'Hi',
+     'body-1' => 'Someone wants to perform a new installation with a previously owned Telraam-device.',
+     'body-2a' => 'The device has serial number ',
+     'body-2b' => 'and to our knowledge registered to ',
+
+    'body-3' => "It’s mandatory to officially stop the previous installation before a new installation can be performed.
+         Please allow this via the button below.",
+
+    'button' => 'Confirm by filling in a small form',
+    'body-4' => 'If this device is unknown to you or have any additional questions feel free to email us at
+      <a href="mailto:support@telraam.net">support@telraam.net</a>',
+
+];
--- lang/en/public-setup.php
+++ lang/en/public-setup.php
@@ -1,34 +1,52 @@
 <?php
 
 return [
-    
+
     'title' => 'Telraam S2',
 
     'subtitle' => 'Gather detailed information on traffic on any street',
     'description' => 'Congratulations on being an S2 host. In the next steps we explain how to instal the device',
-    
+
    'current-installation-title' => 'Current installation of this device',
     'is-counting' => 'Is counting',
     'counted-before' => 'Counted before',
     'no-counting-yet' => 'No counting done yet',
-    
+    'stopped' => 'Device stopped',
+
     'view-the-measurements' => 'View the measurements',
-    
+
     'start-installation-title' => 'Start the installation', //  install with device never used
     'start-installation-not-new-title' => 'More...',
 
     'ready-to-install' => 'You are ready to install the Telraam device on your location?',
-    'ready-to-install-new-owner' => 'You want to install the Telraam device on a new location or it has a new owner?',
-    
+
     'log-in' => 'Log in',
     'or' => 'or',
     'create-an-account' => 'Create an account',
-    
-    'want-to-install-with' => 'Great! I want to install this device with',
+
     'start-setup' => 'Start the setup',
     'want-to-install-with-another-user' => 'I want to install it with another user',
-    
+
     'i-could-use' => 'Oh no, I could use',
    'support' => 'support',
-    'to-get-up' => 'to get this device up and running again.'
+    'to-get-up' => 'to get this device up and running again.',
+
+    // new 12 juni 2024
+    'device-is-not-free'            => 'Device is still owned by somebody',
+    'device-is-not-free-button'     => "Ask the previous owner to officially stop this device.",
+    'device-is-not-free-extra-info' => 'If it\'s your device please login.
+    This action will notify the owner and network manager.',
+    'ready-to-install-new-location' => 'You want to install the Telraam device on a new location?',
+    'want-to-install-with' => 'Great! I want to proceed the installation as ',
+
+    // one network
+    'want-to-install-as-network-admin' => 'I want to proceed the installation as a <i>network admin</i>',
+    'start-setup-as-network-admin-btn'   => 'Start set-up of a self-administered device',
+
+    //
+    'want-to-install-as-network-admin-mulitple-networks' => 'I want to proceed the installation for a self-administered device as a <i>network admin</i>',
+
+
+
+
 ];
