4a54cc444595966e850556a4e68565b8d9a91fd9
Dave Driesmans <dave@waanz.in>
2024-07-23 10:55:12 +0200

    Jobs sales NL

---
+++ lang/en/jobs-sales.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    'title'             => 'Junior Sales Accountmanager for Netherlands',
+  'intro' => 'Local mobility remains one of the most important topics of citizens when liveability is a concern.
+New technological evolutions (IoT, low-cost sensors, edge computing) strengthen the possibilities of
+citizen science and enable citizens to actively contribute to the collection and interpretation of
+data themselves. In this way, citizens can help develop solutions to local mobility problems.<br>
+<br>
+At Telraam, we are empowering citizens to collect traffic data and we are bringing citizens and local governments
+ together. We support the development of local networks of our sensors for the automatic counting of traffic data
+ by citizens in cooperation with local authorities.<br>
+<br>
+Currently Telraam has more than 2000 devices active, mostly in Flanders and to a lesser extent in the Netherlands.
+We want to further establish our presence in the Dutch market and grow the installed base of Telraam sensors, by
+selling networks of sensors to professional clients. We are looking for an enthusiastic and creative person for
+ B2B and B2G sales.',
+
+    'responsibilities-title' => 'Your mission & key responsibilities:',
+    'responsibilities-text' => '<ul>
+<li>Proactively contact potential clients (private/government sector) and scout for grant opportunities in Netherlands;</li>
+<li>You understand and are able to convey our value proposition to the key decision makers how Telraam can solve their
+ challenges;</li>
+<li>Represent Telraam at Dutch events and exhibitions, with a focus to acquire new contacts to convert to sales;</li>
+<li>Build your own pipeline of leads and carry trough from first contact to conversion</li>
+<li>Learn about the Telraam products and services and tailor pitches to potential clients.</li>
+</ul>',
+
+    'profile-title' => 'Profile:',
+    'profile-text' => '<ul>
+<li>Must have: sales mindset with excellent communication and listening skills</li>
+<li>Cultural fit: you firmly believe in the value of citizen science, traffic liveability and open data;</li>
+<li>You have the ability to organise and prioritise your workload independently</li>
+<li>Experience with selling to governmental clients and grants in NL is a plus;</li>
+<li>Experience with sales cycles and working with common CRM tools is a plus;</li>
+<li>Experience with traffic engineering & mobility policy is a plus; </li>
+<li>You function in a comfortable way in a fast-paced, dynamic start-up environment;</li>
+<li>Strong interpersonal skills capable of networking both internally and externally fostering long-lasting
+relationships for the business;</li>
+<li>You’re fluent in Dutch and English; Other languages are an asset</li>
+</ul>',
+
+    'foryou-title' => 'What’s in it for you?',
+    'foryou-text' => '<ul>
+<li>Full-time (flexible);</li>
+<li>You will be part of an exciting, visible and impactful start-up;</li>
+<li>The opportunity to grow and help shape the direction of the start-up;</li>
+<li>A positive and open company culture;</li>
+<li>A competitive salary and benefits package;</li>
+<li>Flexible working hours and ample opportunity to work from home.</li>
+</ul>',
+
+    'closing' => 'Interested? <a mailto:jobs@telraam.net>jobs@telraam.net</a>'
+
+];
---
+++ lang/nl/jobs-sales.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+
+    'title'             => 'Junior Sales Accountmanager voor Nederland',
+  'intro' => 'Lokale mobiliteit blijft een van de belangrijkste onderwerpen van burgers als het gaat om leefbaarheid.
+  Nieuwe technologische evoluties (IoT, goedkope sensoren, edge computing) versterken de mogelijkheden van citizen
+   science en stellen burgers in staat om zelf actief bij te dragen aan het verzamelen en interpreteren van gegevens.
+   Op deze manier kunnen burgers helpen bij het ontwikkelen van oplossingen voor lokale mobiliteitsproblemen.<br>
+<br>
+Bij Telraam stellen we burgers in staat om verkeersgegevens te verzamelen en brengen we burgers en lokale overheden samen.
+We ondersteunen de ontwikkeling van lokale netwerken van onze sensoren voor het automatisch tellen van verkeersgegevens
+ door burgers in samenwerking met lokale overheden.<br>
+<br>
+Momenteel heeft Telraam meer dan 2000 apparaten actief, voornamelijk in Vlaanderen en in mindere mate in Nederland. We
+willen onze aanwezigheid op de Nederlandse markt verder uitbouwen en de installed base van Telraam sensoren laten
+groeien, door netwerken van sensoren te verkopen aan professionele klanten. We zijn op zoek naar een enthousiast en
+creatief persoon voor B2B en B2G sales.<br>
+',
+
+    'responsibilities-title' => 'Je missie en belangrijkste verantwoordelijkheden:',
+    'responsibilities-text' => '<ul>
+<li>Proactief contact opnemen met potentiële klanten (particuliere/overheidssector) en scouten naar
+subsidiemogelijkheden in Nederland;</li>
+<li>Je begrijpt en bent in staat om onze waardepropositie over te brengen aan de belangrijkste beslissingnemers en
+ hoe Telraam hun uitdagingen kan oplossen;</li>
+<li>Telraam vertegenwoordigen op Nederlandse evenementen en beurzen, met een focus op het verwerven van nieuwe
+contacten om deze te converteren naar verkoop;</li>
+<li>Je eigen pijplijn van leads opbouwen en van eerste contact tot conversie begeleiden</li>
+<li>Leren over de producten en diensten van Telraam en pitches op maat maken voor potentiële klanten.</li>
+</ul>',
+
+    'profile-title' => 'Profiel:',
+    'profile-text' => '<ul>
+<li>Must have: sales mindset met uitstekende communicatie- en luistervaardigheden</li>
+<li>Cultural fit: je gelooft sterk in de waarde van citizen science, verkeersleefbaarheid en open data;</li>
+<li>Je hebt het vermogen om zelfstandig je werk te organiseren en prioriteiten te stellen</li>
+<li>Ervaring met verkopen aan overheidsklanten en subsidies in NL is een plus;</li>
+<li>Ervaring met verkoopcycli en werken met gangbare CRM tools is een plus;</li>
+<li>Ervaring met verkeerskunde & mobiliteitsbeleid is een plus; </li>
+<li>Je functioneert op een comfortabele manier in een snelle, dynamische start-up omgeving;</li>
+<li>Je beschikt over sterke interpersoonlijke vaardigheden en bent in staat om zowel intern
+als extern te netwerken en duurzame relaties voor het bedrijf op te bouwen;</li>
+<li>Je spreekt vloeiend Nederlands en Engels; Andere talen zijn een pluspunt</li>
+</ul>',
+
+    'foryou-title' => 'What’s in it for you?',
+    'foryou-text' => '<ul>
+<li>Fulltime (flexibel);</li>
+<li>Je maakt deel uit van een spannende, zichtbare en impactvolle start-up;</li>
+<li>De kans om te groeien en de richting van de start-up mee vorm te geven;</li>
+<li>Een positieve en open bedrijfscultuur;</li>
+<li>Een competitief salaris en voordelenpakket;</li>
+<li>Flexibele werktijden en voldoende mogelijkheden om thuis te werken.</li>
+</ul>',
+
+    'closing' => 'Geïnteresseerd? <a mailto:jobs@telraam.net>jobs@telraam.net</a>'
+
+];
