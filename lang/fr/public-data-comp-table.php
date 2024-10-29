b3d8803f96a3e1ce5301b13520db0780b11f6cfe
lucsegers <89685548+lucsegers@users.noreply.github.com>
2024-10-29 08:59:21 +0100

    Update public-data-comp-table.php

--- lang/fr/public-data-comp-table.php
+++ lang/fr/public-data-comp-table.php
@@ -10,24 +10,24 @@ return [
     'accessible-with' => 'Accessible avec',
 
     'header-public' => '<strong>SUR LA PAGE PUBLIQUE</strong><br> disponible pour tout le monde',
-    'header-dashboard' => '<strong>DANS VOTRE TABLEAU DE BORD PERSONNEL</strong><br> disponible pour les propriétaires de Telraam',
+    'header-dashboard' => '<strong>SUR VOTRE TABLEAU DE BORD PERSONNEL</strong><br> disponible pour les propriétaires de Telraam',
 
-    'public-feat-1' => 'Comptages & direction de voyage pour 4 catégories',
+    'public-feat-1' => 'Comptages & sens de circulation pour 4 catégories',
     'public-feat-2' => 'Comptages de nuit',
     'public-feat-3' => 'Aperçus quotidiens et horaires, vitesse, répartition modale',
     'public-feat-4' => 'Classement',
 
     'sub-title' => 'Obtenez un aperçu direct de la circulation dans votre rue.<br>
-Recevez les comptages des voitures, vélos, piétons, poids lourds et plus encore !',
+Recevez les comptages de voitures, vélos, piétons, poids lourds et plus encore !',
 
-    'dashboard-feat-1' => 'Sélection de l’intervalle de dates flexible',
-    'dashboard-feat-2' => 'Rapport de circulation mensuel',
-    'dashboard-feat-3' => 'Exporter les données brutes',
-    'dashboard-feat-4' => 'Comptages & direction de voyage pour 10 catégories',
+    'dashboard-feat-1' => 'Sélection de plage de dates flexible',
+    'dashboard-feat-2' => 'Rapport mensuel de circulation',
+    'dashboard-feat-3' => 'Export de données brutes',
+    'dashboard-feat-4' => 'Comptages & sens de circulation pour 10 catégories',
     'dashboard-feat-5' => 'Tendances',
     'dashboard-feat-6' => 'Données par intervalle de 15 minutes',
     'dashboard-feat-7' => 'Données par installation',
-    'dashboard-feat-8' => 'Exporter la "feuille de rue"',
+    'dashboard-feat-8' => 'Export de la "feuille de rue"',
     'dashboard-feat-9' => 'Accès API de base',
     'dashboard-feat-10' => 'Accès API avancé',
 
