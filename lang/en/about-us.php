f1322da0cff7221bab72b74b6428ebfaba5707c9
Telraam <48987469+Telraam@users.noreply.github.com>
2025-01-06 13:48:19 +0100

    Update about-us.php
    Updated Peter's info.

--- lang/en/about-us.php
+++ lang/en/about-us.php
@@ -62,9 +62,8 @@ human-focused, multimodal traffic culture. </p>',
     'sentence-dave'    => 'Likes to have design, development and a bit of strategy all mixed in his work.
     Enjoys art, music and a bit of crazyness. Plays outdoor football till his knees really give it up.',
 
-    'role-peter'    => 'Data-analytics and development',
-    'sentence-peter'    => 'Peter has a PhD in Astronomy and Astrophysics (KU Leuven), and 5 years of experience in working with transport related data.
-    After work he is a road cyclist, a private pilot, or a bass player. YNWA!',
+    'role-peter'    => 'Chief Tedhnology Officer',
+    'sentence-peter'    => 'Péter has a PhD in Astronomy and Astrophysics (KU Leuven), and many years of experience in working with transport related data (and AWS). He loves getting lost in graphic/product design too. After work he is either cycling, fly fishing, flying as a private pilot, or playing the bass. YNWA!',
 
     'role-luc'     => 'Embedded software engineer',
     'sentence-luc'    => 'Started working on embedded systems 20+ years ago and since then
