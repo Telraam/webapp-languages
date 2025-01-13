10888cf47b0a7d182493fe04e10cf9776ab2e144
Dave Driesmans <dave@waanz.in>
2025-01-13 10:11:17 +0100

    Outdoor

--- lang/en/telraamOutdoor.php
+++ lang/en/telraamOutdoor.php
@@ -17,23 +17,61 @@ develop an autonomous Telraam S2 device which you can install outdoors. We want 
  and combine it with a small solar panel and internal battery. Because our device consumes very little power, we get
  by with a small solar panel & battery.</p>',
 
-    // timeline
-    'timeline-step1-title'        => 'Design & development',
-    'timeline-step2-title'        => 'Limited deployment in 2 pilot projects (Belgium)',
-    'timeline-step2-subtitle'     => 'In co-operation with EiT and Provincie Vlaams Brabant',
-    'timeline-step3-title'        => 'Start market roll-out (Belgium) + start pilot projects (internationally)',
+    // table
+    'row-1-header'  => 'Hardware price',
+    'row-1-s2'      => '€ 160.00 <span class="grey">excl vat</span>',
+    'row-1-outdoor' => '€ 500.00 <span class="grey">excl vat</span>',
+
+    'row-2-header'  => 'Subscription',
+    'row-2-s2'      => 'Free / Premium / Network',
+    'row-2-outdoor' => 'Network only',
+
+    'row-3-header'  => 'Installation & maintenance',
+    'row-3-s2'      => 'Citizen',
+    'row-3-outdoor' => 'Professional',
+
+    'row-4-header'  => 'Power',
+    'row-4-s2'      => 'Standard plug with USB-C',
+    'row-4-outdoor' => 'DC connection <br>Optional solar panel & battery',
+
+    'row-5-header'  => 'Positioning',
+    'row-5-s2'      => 'Window, indoors',
+    'row-5-outdoor' => 'Suitable elevated location outdoors (posts, buildings & structures, trees, etc.)',
+
+    'row-6-header'  => 'Design',
+    'row-6-s2'      => 'Attractive design with interactive LCD display & online dashboard',
+    'row-6-outdoor' => 'Unobtrusive weatherproof enclosure & online dashboard controls',
+
+    // block more info: Why choose Telraam S2 Outdoor?
+    'section-more-title' => 'Why choose Telraam S2 Outdoor?',
+
+    'section-more-block-1-title' => 'Low power',
+    'section-more-block-1-body' => 'Telraam S2 has been designed to use very little power. The outdoor unit consumes less than 0.8W so can easily be supported by existing connections if required.
+The Telraam Outdoor unit is DC powered. A built-in DC convertor only requires 12-24V power input.',
+
+    'section-more-block-2-title' => 'Multimodal',
+    'section-more-block-2-body' => 'All Telraam S2 devices count, categorise, and report all activity on the street: cars, bikes, pedestrians, buses, trucks, and more.
+Placing the device outdoor allows a wider range of counting locations in addition to residential streets.',
+
+    'section-more-block-3-title' => 'Easy installation',
+    'section-more-block-3-body' => 'Telraam devices are designed to be user-friendly and easy to install and performance can be monitored using an online platform.
+Technical expertise is only required to supply the outdoor device with power, but a manual and support materials are included.',
+
+    'section-more-block-4-title' => 'Privacy',
+    'section-more-block-4-body' => 'Telraam devices use AI to process video footage locally on the device. Only traffic counts and categorized data (e.g., cars, pedestrians, bicycles) are transmitted to the cloud and the raw video footage is not stored or transmitted.',
+
 
     // FAQ
     'faq-title'         => 'FAQ',
     'question-1'           => 'Is there a difference between the regular S2 and this outdoor device?',
-	'answer-1'           => 'The internal components remain the same, but the outer casing has been updated to be weather-resistant.
+    'answer-1'           => 'The internal components remain the same, but the outer casing has been updated to be weather-resistant.
  The device functions just like the regular S2 for indoor use and runs the same software to detect traffic. This means it has the same <a href="https://faq.telraam.net/article/3/requirements-for-the-installation-of-a-telraam-in-my-window">location requirements</a> as the indoor Telraam S2.',
 
-    'question-2'           => 'What will the cost be?',
- 	'answer-2' 			=> 'The cost of the device will be significantly compared to the Telraam S2 indoor model. The solar panel is not included in our offering and can be purchased at an additional price, in Belgium only. We do not ship the solar+battery kit internationally.',
+    'question-2'           => 'What is the cost?',
+    'answer-2' 			=> 'The price of the S2 Outdoor device is 500eur. The solar panel is not included in our offering and can be purchased at an additional price, in Belgium only. We do not ship the solar+battery kit internationally.',
 
- 	'question-3'           => ' Is the device fully power independent?',
- 	'answer-3' 			=> 'Almost. You can get 100% full autonomy with a large solar panel + battery, but there\'s a sensible trade-off
+    'question-3'           => ' Is the device fully power independent?',
+    'answer-3' 			=> 'Almost. You can get 100% full autonomy with a large solar panel + battery, but there\'s a sensible trade-off
  	between size of battery and solar panel and the cost. We believe we found the right balance during the prototype phase.'
 
 ];
