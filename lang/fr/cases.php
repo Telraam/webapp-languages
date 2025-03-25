01b2aa3a197cf3c691d44353b572804a06e07164
Dave Driesmans <dave@waanz.in>
2025-03-25 16:46:10 +0100

    cases

---
+++ lang/fr/cases.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    // highlighted block key elements
+    'city'                     => 'City',
+    'population'               => 'Population',
+    'key-information'          => 'Key information',
+    'client'                   => 'Client',
+    'services-provided'        => 'Services provided',
+    'number-telraam-devices'   => 'Number Telraam devices',
+    'use-cases'                 => 'Use cases',
+    'responsible'               => 'Responsible',
+    'project-duration'         => 'Project duration',
+    'project-budget'            => 'Project budget (range)',
+
+    // clients
+    'local_authority'       => 'Local authority',
+    'ngo'                   => 'NGO or advocacy group',
+    'institute'             => 'Knowledge/Research institute',
+    'consulting_company'    => 'Consulting company',
+    'commercial_partner'    => 'Commercial partner',
+    'other'                 => 'Other',
+
+    // services provided
+    'sensor_and_data_hosting'       => 'Sensor and data hosting',
+    'sensor_installations'          => 'Sensor installations',
+    'consultancy'                   => 'Data analysis (consultancy)',
+    'Community engagement (workshops, ...)'    => 'Community engagement (workshops, ...)',
+    'custom'                        => 'Custom development (sensor, dashboard, API, ...)',
+
+    // use cases
+    'longterm_monitoring'       => 'Long-term monitoring',
+    'citizen_engagement'        => 'Citizen Engagement',
+    'speed_monitoring'          => 'Speed monitoring',
+    'infrastructure'            => 'Infrastructure implementation (bike lanes, ...)',
+    'traffic_safety'            => 'Traffic safety',
+    'footfall_monitoring'       => 'Footfall monitoring',
+    'school_street'             => 'School street',
+    'parking'                   => 'Parking',
+    'sump'                      => 'SUMP',
+    'tourism_impact'            => 'Tourism impact',
+    'air_quality'               => 'Air quality',
+
+    // budget
+    'small'         => 'Small (5k€ - 10k€)',
+    'medium'        => 'Medium (10k€ - 30k€)',
+    'large'         => 'Large (30k€ - 100k€)',
+    'research'      => 'Research (>100k€)',
+
+];
---
+++ lang/nl/cases.php
@@ -0,0 +1,50 @@
+<?php
+
+return [
+    // highlighted block key elements
+    'city'                     => 'Stad',
+    'population'               => 'Bevolking',
+    'key-information'          => 'Informatie',
+    'client'                   => 'Klant',
+    'services-provided'        => 'Geleverde diensten',
+    'number-telraam-devices'   => 'Aantal Telraam-toestellen',
+    'use-cases'                 => 'Use cases',
+    'responsible'               => 'Verantwoordelijke',
+    'project-duration'         => 'Projectduur',
+    'project-budget'            => 'Projectbudget',
+
+    // clients
+    'local_authority'       => 'Lokale overheid',
+    'ngo'                   => 'NGO of belangenorganisatie',
+    'institute'             => 'kennis-/onderzoeksinstituut',
+    'consulting_company'    => 'Studiebureau',
+    'commercial_partner'    => 'Commerciële partner',
+    'other'                 => 'Andere',
+
+    // services provided
+    'sensor_and_data_hosting'       => 'Sensor- en datahosting',
+    'sensor_installations'          => 'Sensorinstallaties',
+    'consultancy'                   => 'Data analyse (consultancy)',
+    'Community engagement (workshops, ...)'    => 'Community engagement (workshops, ...)',
+    'custom'                        => 'Ontwikkeling op maat (sensor, dashboard, API, ...)',
+
+    // use cases
+    'longterm_monitoring'       => 'Langetermijnmonitoring',
+    'citizen_engagement'        => 'Burgerbetrokkenheid',
+    'speed_monitoring'          => 'Snelheidsmonitoring',
+    'infrastructure'            => 'Infrastructuur implementatie (fietspaden, ...)',
+    'traffic_safety'            => 'Verkeersveiligheid',
+    'footfall_monitoring'       => 'Voetvalmonitoring',
+    'school_street'             => 'Schoolstraat',
+    'parking'                   => 'Parkeren',
+    'sump'                      => 'SUMP',
+    'tourism_impact'            => 'Toeristische impact',
+    'air_quality'               => 'Luchtkwaliteit',
+
+    // budget
+    'small'         => 'Klein (5k€ - 10k€)',
+    'medium'        => 'Medium (10k€ - 30k€)',
+    'large'         => 'Groot (30k€ - 100k€)',
+    'research'      => 'Onderzoek (>100k€)',
+
+];
