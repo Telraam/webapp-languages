3b26fa4faaf4719fcf13885783bbef1f53180071
Dave Driesmans <dave@waanz.in>
2025-06-20 14:23:27 +0200

    tokens

---
+++ lang/en/tokens.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+
+
+    'overview-info-box'                => 'Our API allows a developer to read the data and reports for the available Telraam street segments.
+                        The API token is a unique identifier to request access to the telraam api service.
+                         You can find the documentation on <a href="https://telraam-api.net/">the dedicated api website</a>.',
+
+
+    // create a token
+    'please-make-your-selection' => '-- Please make your selection --',
+
+    'q1' => 'What is your main reason for using the Telraam API?',
+    'q1-datageek' => 'I’m a data geek — just curious to play with the data. 100% non-professional use.',
+    'q1-traffic-data' => 'I work with traffic data professionally and want to explore Telraam data out of interest.',
+    'q1-commercial-test' => 'I have a specific commercial idea and want to test the API and experiment a bit.',
+    'q1-commercial' => 'I have a specific commercial idea',
+    'q1-researcher' => 'I’m a researcher (university or knowledge institute) and plan to use the data in a research project.',
+    'q1-other' => 'Other',
+
+    'q2'            => 'How many sensors do you expect to query via the API?',
+    'q2-justmyown'  => 'Just my own sensor',
+    'q2-few'        => 'A few — 10 or fewer',
+    'q2-specific'   => 'A specific area (city, region, or country) — possibly several hundred',
+    'q2-all'        => 'All of them — the more the merrier!',
+
+    'q3'            => 'How would you like us to be involved (if at all)?',
+    'q3-fine'       => 'I’m good — the documentation on telraam-api.net is enough',
+    'q3-needsupport'  => 'I might need some help; I’ll check Telraam Talks or reach out via support@telraam.net',
+    'q3-custom'       => 'I may need a custom API endpoint or development support from Telraam (consultancy assignment)',
+
+    'contact-details' => 'Contact Details',
+    'name' => 'Name',
+    'company' => 'Company',
+    'website' => 'Website',
+
+    'acknowledge' => 'I acknowledge the Telraam API is provided under a CC-BY-NC license. I will only use it for non-commercial purposes
+    and will attribute Telraam properly. I understand Telraam may revoke access if these terms are breached. If I plan to build
+    something commercial, I’ll contact Telraam first via info@telraam.net.',
+
+    'request' => 'Create token',
+
+];
